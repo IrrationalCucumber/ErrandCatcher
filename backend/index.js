135466fefaa535207b6b29f4c64851ebab4dad5f
adrean paul sorono <adreanpaulsorono@gmail.com>
2023-08-20 16:53:42 +0800

    complete sign-in

--- backend/index.js
+++ backend/index.js
@@ -63,36 +63,6 @@ app.get("/search-commission", (req, res) => {
         return res.json(data);
     });
 });
-//sign-in
-app.get("/sign-in", (req, res) => {
-    const username = req.query.username;
-    const password = req.query.password;
-    const q = "SELECT * FROM UserAccount WHERE (username = ? OR userEmail = ?) AND password = ?";
-  
-    db.query(q, [username, username, password], (err, data) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ error: 'An error occurred' });
-      }
-      return res.json(data);
-    });
-  });
-  
-
-//home based on id
-app.get("/home/:userID", (req, res) => {
-    const userID = req.params.userID; // Get the search term from the query parameter
-    const q = "SELECT * FROM commission WHERE userID = ?";
-
-    db.query(q, [userID], (err, data) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ error: 'An error occurred' });
-        }
-        return res.json(data);
-    });
-});
-
 
 //send data to userAccount
 app.post("/user", (req,res)=>{
@@ -159,3 +129,33 @@ app.delete("/commission/:commissionID", (req, res)=>{
 app.listen(8800, ()=>{
     console.log("connected to backend!")
 })
+
+//sign-in
+app.get("/sign-in", (req, res) => {
+    const username = req.query.username;
+    const password = req.query.password;
+    const q = "SELECT * FROM UserAccount WHERE (username = ? OR userEmail = ?) AND password = ?";
+  
+    db.query(q, [username, username, password], (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'An error occurred' });
+      }
+      return res.json(data);
+    });
+  });
+  
+
+//home based on id
+app.get("/home/:userID", (req, res) => {
+    const userID = req.params.userID; // Get the search term from the query parameter
+    const q = "SELECT * FROM commission WHERE userID = ?";
+
+    db.query(q, [userID], (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'An error occurred' });
+        }
+        return res.json(data);
+    });
+});
