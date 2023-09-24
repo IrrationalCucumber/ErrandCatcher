eef9de6125c71afa2cab758f2d04ed2f0d05b723
adrean paul sorono <adreanpaulsorono@gmail.com>
2023-09-24 14:40:37 +0800

    added back button

--- client/src/components/NavBarPage.js
+++ client/src/components/NavBarPage.js
@@ -1,55 +1,59 @@
-import React, {useState, useEffect} from 'react'
-import { Link } from 'react-router-dom'
-import { Button } from './Button';
-import './Navbar.css'
-
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "./Button";
+import "./Navbar.css";
 
 function Navbar() {
   //change the state of the menu
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
-//reverse the state of the above funstion
+  //reverse the state of the above funstion
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
   const showButton = () => {
-    if(window.innerWidth <= 960){
-      setButton(false)
-    } else{
-      setButton(true)
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
     }
-  }
+  };
 
   useEffect(() => {
-    showButton()
-  }, [])
+    showButton();
+  }, []);
 
-//handles the resizing of window
-window.addEventListener('resize', showButton)
+  //handles the resizing of window
+  window.addEventListener("resize", showButton);
+
+  const navigate = useNavigate();
+
+  //function for my back
+  const goBack = () => {
+    navigate(-1); // This navigates back to the previous page
+  };
 
   return (
     <>
-        <nav className="navbar">
-            <div className="navbar-container">
-                <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-                    ERRAND CATCHER<i className="fab fa-typo3"></i>
-                </Link>
-                <div className='menu-icon' onClick={handleClick}>
-                  <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
-                </div>
-                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                  <li className='nav-item'>
-                   
-                  </li>
-                </ul>
-                {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
-            </div>
-        </nav>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link className="navbar-logo" onClick={goBack}>
+            BACK<i className="fab fa-typo3"></i>
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item"></li>
+          </ul>
+          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
+        </div>
+      </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
 /**
  * <li className='nav-item'>
                     <Link to='/commission' className='nav-links' onClick={closeMobileMenu}>
