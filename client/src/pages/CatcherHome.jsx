bf0985f8db768c524e40df6fa8c21993ffd2c863
adrean paul sorono <adreanpaulsorono@gmail.com>
2023-10-05 10:45:10 +0800

    delete unused components

--- client/src/pages/CatcherHome.jsx
+++ client/src/pages/CatcherHome.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
-import Navbar from '../components/CatcherNavbar'
-import Cards from '../components/Cards'
+import React from "react";
+import NavBar from "../components/Navbar";
+import Cards from "../components/Cards";
+import { useLocation } from "react-router-dom";
 
 function CatcherHome() {
+  //get the id from the address bar
+  const location = useLocation();
+  const userID = location.pathname.split("/")[2];
   return (
     <div>
-      <Navbar></Navbar>
-        <Cards></Cards>
+      <NavBar
+        page1="HOME"
+        home={`/e-home/${userID}`}
+        page2="COMMISSIONS"
+        commissionList={`/commissions/${userID}`}
+        page3="APPLICANTS"
+        applicants={`/applicants/${userID}`}
+        map={`/map/${userID}`}
+        button="SIGN OUT"
+      />
+      <Cards></Cards>
     </div>
-  )
+  );
 }
 
-export default CatcherHome
+export default CatcherHome;
