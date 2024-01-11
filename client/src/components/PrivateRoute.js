3d2631643d5ed3de7cbcf6a0c7be4c2adcd7136f
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-01-11 17:22:48 +0800

    components for si/so restriction

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,22 @@
+// PrivateRoute.js
+import React from "react";
+import { useAuth } from "./AuthContext";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+const PrivateRoute = ({ path, ...props }) => {
+  const { isLoggedIn } = useAuth();
+
+  if (!isLoggedIn) {
+    // Redirect to the login page if the user is not authenticated
+    return <Navigate to="/sign-in" />;
+  }
+
+  // If authenticated, render the Route component
+  return (
+    <Routes>
+      <Route path={path} {...props} />
+    </Routes>
+  );
+};
+
+export default PrivateRoute;
