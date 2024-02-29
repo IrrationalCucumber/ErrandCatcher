f4ea2c6aae5c1dbffeed42635e15a1004eec861f
it-elect 4 <trishamaesasing64@gmail.com>
2024-02-29 22:40:09 +0800

    frontend-footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,55 +7,17 @@ function Footer() {
   return (
     <div className='footer-container'>
         <section className='footer-subscription'>
-            <p className='footer-subscription-heading'>
-                Join the Adventure newsletter to receive our best vacation deals
-            </p>
-            <p className='footer-subscription-text'>
-                You can unsubscribe at any time.
-            </p>
+            
+           
             <div className='input-areas'>
-                <form>
-                    <input type='email' name='email' placeholder='Your Email' className='footer-input'/>
-                    <Button buttonStyle='btn--outline'>Subscribe</Button>
-                </form>
+
             </div>
         </section>
         <div className='footer-links'>
             <div className='footer-link-wrapper'>
                 <div className='footer-link-items'>
-                    <h2>About Us</h2>
-                    <Link to='/sign-up'>How it works</Link>
-                    <Link to='/'>Testimonials</Link>
-                    <Link to='/'>Careers</Link>
-                    <Link to='/'>Investors</Link>
-                    <Link to='/'>Terms of Service</Link>
-                </div>
-            </div>
-            <div className='footer-link-wrapper'>
-                <div className='footer-link-items'>
-                    <h2>Contact Us</h2>
-                    <Link to='/'>Contact</Link>
-                    <Link to='/'>Support</Link>
-                    <Link to='/'>Destinations</Link>
-                    <Link to='/'>Sponsorship</Link>
-                </div>
-            </div>
-            <div className='footer-link-wrapper'>
-                <div className='footer-link-items'>
-                    <h2>Videos</h2>
-                    <Link to='/'>Submit Video</Link>
-                    <Link to='/'>Ambassadors</Link>
-                    <Link to='/'>Agency</Link>
-                    <Link to='/'>Influencer</Link>
-                </div>
-            </div>
-            <div className='footer-link-wrapper'>
-                <div className='footer-link-items'>
-                    <h2>Social Media</h2>
-                    <Link to='/'>Instagram</Link>
-                    <Link to='/'>Facebook</Link>
-                    <Link to='/'>Youtube</Link>
-                    <Link to='/'>Twitter</Link>
+                    <h2>Errand Catcher</h2>
+                
                 </div>
             </div>
         </div>
@@ -63,7 +25,12 @@ function Footer() {
             <div className='social-media-wrap'>
                 <div className='footer-logo'>
                     <Link to='/' className='social-logo'>
-                        ERRAND CATCHER <i fab fab-typo3></i>
+                        About Us <i fab fab-typo3></i>
+                    </Link>
+                </div>
+                <div className='footer-logo'>
+                    <Link to='/' className='social-logo'>
+                        Contact Us <i fab fab-typo3></i>
                     </Link>
                 </div>
                 <small className='website-rights'>ERRAND CATCHER</small>
