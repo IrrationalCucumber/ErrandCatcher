ac93cc308c949df8741d3e9ce2df9c9c0e856592
Lorry Mae <halinalorr19@gmail.com>
2024-03-07 16:57:44 +0800

    Notification

--- client/src/pages/Notification.jsx
+++ client/src/pages/Notification.jsx
@@ -1,64 +1,91 @@
-/**------------NOTE--------------------
- * this could be tranferred to/chaanged as 'Notification' page
- */
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import "./Notification2.css"
+import Navbar from "../components/Navbar";
 import NotificationItem from "../components/NotificationItem";
-import { useLocation } from "react-router-dom";
-import "../components/Notification.css";
 
-function Notification() {
-  const [notifs, setNotifs] = useState([]);
 
-  //get the id from address bar
-  const location = useLocation();
-  const userID = location.pathname.split("/")[2];
-  //display all notification
-  useEffect(() => {
-    const fetchNotif = async () => {
-      try {
-        //const res = await axios.get(`http://localhost:8800/show-notifs/${userID}`);
-        const res = await axios.get("http://localhost:8800/notification");
-        setNotifs(res.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchNotif();
-  }, []);
-  //when user click 'mark as read'
-  //update db notif isRead to Yes
-  const ReadIt = async (e) => {
-    try {
-      await axios.post("http://localhost:8800/read-notif/" + notifs, userID);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+export default function Notification(){
 
-  return (
-    <div className="notifs">
-      <div className="notifs_container">
-        <div className="notifs_wrapper">
-          {notifs.map((Notif) => (
-            <div className="notif" key={Notif.notificationID}>
-              <ul className="notifs_items">
-                <NotificationItem
-                  type={Notif.notificationType}
-                  desc={Notif.notifDesc}
-                  date={Notif.notifDate}
-                  // type="APPLICATION"
-                  // desc="There is an Catcher that wants to apply on one of your commission"
-                  // date="3:00 11-11-11"
-                  reddit={ReadIt}
-                />
-              </ul>
+    const notificationData = [
+        {
+            id: 1,
+            name: "Jhon Doe",
+            notifMessage: "posted an errand",
+            postMessage:"Looking for a plumber",
+            time: "10 mins ago",
+            postImg: "/images/post1.jpg",
+            profileImg: "/images/profile1.jpg"
+        },
+        {
+            id: 1,
+            name: "Richard Miles",
+            notifMessage: "Approved your application",
+            postMessage:"Your application is approved please go to the said location.",
+            time: "10 mins ago",
+            postImg: "/images/post2.jpg",
+            profileImg: "/images/profile2.jpg"
+        }
+    ]
+
+    const cardList = notificationData.map(data =>([
+        <NotificationCard 
+            key={data.id}
+            name={data.name}
+            notifMessage={data.notifMessage}
+            time={data.time}
+            postImg={data.postImg}
+            postMessage={data.postMessage}
+            profileImg={data.profileImg}
+        />
+    ]))
+
+    function NotificationCard(props){
+        return(
+            <div className="notif-card-container">
+               <div className="notif-card-content">
+                    <img src={props.profileImg} className="notif-img" alt="profile"/>
+                    <div className="notif-details">
+                        <div style={{
+                            display: "flex",
+                            gap: "5px"
+                        }}>
+                            <p style={{fontWeight: "bold"}} className="notif-name">{props.name}</p>
+                            <p className="notif-message">{props.notifMessage}</p>
+                        </div>
+                        <p className="notif-post-message">{props.postMessage}</p>
+                        <p className="notif-time">{props.time}</p>
+                    </div>
+                    <img src={props.postImg} className="post-img" alt="post"/>
+               </div>
             </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-}
+        )
+    }
 
-export default Notification;
+
+    return(
+        <div className="notification2-container">
+            <Navbar
+        page1="HOME" 
+        home={`/e-home`}
+        page2="COMMISSIONS"
+        commissionList={`/commissions`}
+        page3="APPLICANTS"
+        applicants={`/applicants`}
+        map={`/map`}
+        button="SIGN OUT"
+        pageButton="/sign-in"
+      />
+            <main className="notification-main">
+                <div style={{
+                    display: "flex",
+                    gap: "10px"
+                }}>
+                    <p className="notification-title">Notifications</p>
+                    <img src="/images/notification_icon.svg" className="icon" alt="notification_icon"/>
+                </div>
+                <div className="notification-list">
+                    {cardList}
+                </div> 
+            </main>
+        </div>
+    )
+}
