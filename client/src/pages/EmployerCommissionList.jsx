aa6419b8b70cd4f21a3184af8eb81b8e35cf0dcb
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-03-12 13:50:34 +0800

    initial filter function

--- client/src/pages/EmployerCommissionList.jsx
+++ client/src/pages/EmployerCommissionList.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import axios from "axios";
 import NavBar from "../components/Navbar.js";
-import "./ecommission.css"
+import "./ecommission.css";
 import Table from "../components/Table.js";
 import Pagination from "../components/Pagination.js";
 
@@ -12,11 +12,14 @@ const CommissionList = () => {
   const [searchTerm, setSearchTerm] = useState("");
 
   const location = useLocation();
+  // state for Filter%search
+  const [type, setType] = useState("");
+  const [status, setStatus] = useState("");
 
   // Pagination
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState(10);
-  
+
   const userID = location.pathname.split("/")[2];
   //pathname to array from
 
@@ -56,6 +59,29 @@ const CommissionList = () => {
     fetchSearchResults();
   }, [searchTerm]); // Trigger the search whenever searchTerm changes
 
+  // APS - 12/03/24
+  // // filter type //
+  const fetchType = async () => {
+    try {
+      const res = await axios.get(
+        `http://localhost:8800/filter-type/${userID}`,
+        {
+          params: {
+            //type: type,
+            status: status,
+          }, // Pass the search term as a query parameter
+        }
+      );
+      setAccounts(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchType();
+  }, [type, status]); // reflect changes
+
   //funtion to delete commission
   const handleDelete = async (commissionID) => {
     try {
@@ -67,7 +93,7 @@ const CommissionList = () => {
       console.log(err);
     }
   };
-  
+
   // Pagination functions
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
   const indexOfLastItem = currentPage * itemsPerPage;
@@ -87,65 +113,79 @@ const CommissionList = () => {
         map={`/map/${userID}`}
         button="SIGN OUT"
       />
-      <div className='Commission-page-container'>
-      <div className='Commission-page'>
-      <h1>Commission List</h1>
-      <div className="commissions">
-      <div className="search-filter">
-          <div className="search">
-            <input
-              type="text"
-              placeholder="Search..."
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
+      <div className="Commission-page-container">
+        <div className="Commission-page">
+          <h1>Commission List</h1>
+          <div className="commissions">
+            <div className="search-filter">
+              <div className="search">
+                <input
+                  type="text"
+                  placeholder="Search..."
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <button type="submit">
+                  <i className="fa fa-search"></i>
+                </button>
+              </div>
+              <div className="filter">
+                <select>
+                  <option value="">All Status</option>
+                  <option value="Pending">Pending</option>
+                  <option value="Completed">Completed</option>
+                  <option value="Cancelled">Cancelled</option>
+                </select>
+              </div>
+            </div>
+
+            <Table
+              headers={[
+                "ID",
+                "CATCHER",
+                "ERRAND TITLE",
+                "DATE POSTED",
+                "STATUS",
+                "ACTION",
+              ]}
+              data={currentItems.map((commissionItem) => [
+                commissionItem.commissionID,
+                commissionItem.employerID,
+                commissionItem.commissionTitle,
+                commissionItem.DatePosted,
+                commissionItem.commissionStatus,
+                <React.Fragment>
+                  <button
+                    onClick={() => handleDelete(commissionItem.commissionID)}
+                  >
+                    DELETE
+                  </button>
+                  <button className="update">
+                    <Link
+                      to={`/update-commission/${commissionItem.commissionID}/${userID}`}
+                    >
+                      View
+                    </Link>
+                  </button>
+                </React.Fragment>,
+              ])}
             />
-            <button type="submit">
-              <i className="fa fa-search"></i>
-            </button>
-          </div>
-          <div className="filter">
-            <select>
-              <option value="">All Status</option>
-              <option value="Pending">Pending</option>
-              <option value="Completed">Completed</option>
-              <option value="Cancelled">Cancelled</option>
-            </select>
           </div>
         </div>
-
-        <Table
-            headers={['ID', 'CATCHER', 'ERRAND TITLE', 'DATE POSTED', 'STATUS', 'ACTION']}
-            data={currentItems.map((commissionItem) => ([
-              commissionItem.commissionID,
-              commissionItem.employerID,
-              commissionItem.commissionTitle,
-              commissionItem.DatePosted,
-              commissionItem.commissionStatus,
-              <React.Fragment>
-                <button onClick={() => handleDelete(commissionItem.commissionID)}>DELETE</button>
-                <button className="update">
-                  <Link to={`/update-commission/${commissionItem.commissionID}/${userID}`}>View</Link>
-                </button>
-              </React.Fragment>
-         ]))}
-        />
-      </div>
-      </div>
-            {/* Pagination controls */}
-            {commissions.length > 0 && (
-        <Pagination
-          itemsPerPage={itemsPerPage}
-          totalItems={commissions.length}
-          paginate={paginate}
-        />
-      )}
+        {/* Pagination controls */}
+        {commissions.length > 0 && (
+          <Pagination
+            itemsPerPage={itemsPerPage}
+            totalItems={commissions.length}
+            paginate={paginate}
+          />
+        )}
       </div>
       <button className="add-errand">
         <Link to={`/post-commission/${userID}`}>
           <i className="fa-solid fa-plus"></i> Add Errand
         </Link>
       </button>
-
     </div>
   );
 };
