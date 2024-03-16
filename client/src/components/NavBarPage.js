79bb1aeef858d6c7c432213ca625db2bb3973048
AshleyBelle <113760550+AshleyBelle@users.noreply.github.com>
2024-03-16 13:30:09 +0800

    Revert "navbar"

--- client/src/components/NavBarPage.js
+++ client/src/components/NavBarPage.js
@@ -38,7 +38,7 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link className="navbar-logo" onClick={goBack}>
-            <i class="fa-solid fa-arrow-left"></i>
+            BACK
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
