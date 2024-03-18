e35ef05932ff6a7e90d747476524e0c5ebda305b
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-03-18 10:27:45 +0800

    fixed import name

--- client/src/pages/ViewCommission.jsx
+++ client/src/pages/ViewCommission.jsx
@@ -1,47 +1,47 @@
-import axios from 'axios'
-import React, { useState, useEffect } from 'react'
-import { useNavigate, Link, useLocation } from 'react-router-dom'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useNavigate, Link, useLocation } from "react-router-dom";
 import Navbar from "../components/NavBarPage";
 import maplibregl from "maplibre-gl";
-import Map from '../components/Map';
-import '../style.css'
-import './Commission.css'
+import Map from "../components/Map";
+import "../style.css";
+import "./Commission.css";
 
 const ViewCommission = () => {
   const [commission, setCommission] = useState({
-    comID: '',
-    comTitle: '',
-    comDeadline: '',
-    comLocation: '',
-    comType: '',
-    comDescription: '',
-    comPay: '',
-    comStatus: '',
-    catcherID: '',
+    comID: "",
+    comTitle: "",
+    comDeadline: "",
+    comLocation: "",
+    comType: "",
+    comDescription: "",
+    comPay: "",
+    comStatus: "",
+    catcherID: "",
     //DatePosted:"",
-    DateCompleted: '',
-    ContactNo: ''
-  })
+    DateCompleted: "",
+    ContactNo: "",
+  });
 
-  const navigate = useNavigate()
-  const location = useLocation()
+  const navigate = useNavigate();
+  const location = useLocation();
   //pathname to array from
   //get the id
-  const commissionID = location.pathname.split('/')[2]
-  console.log(location.pathname.split('/')[2])
+  const commissionID = location.pathname.split("/")[2];
+  console.log(location.pathname.split("/")[2]);
 
   //handle changes
-  const handleChange = e => {
-    if (e.target.name === 'comType') {
-      setCommission(prev => ({ ...prev, comType: e.target.value }))
+  const handleChange = (e) => {
+    if (e.target.name === "comType") {
+      setCommission((prev) => ({ ...prev, comType: e.target.value }));
       //setImageURL(commissionTypeImages[e.target.value]);
-    } else if (e.target.name === 'comDescription') {
-      setCommission(prev => ({ ...prev, comDescription: e.target.value }))
+    } else if (e.target.name === "comDescription") {
+      setCommission((prev) => ({ ...prev, comDescription: e.target.value }));
     } else {
       // For other fields, use spread syntax as before
-      setCommission(prev => ({ ...prev, [e.target.name]: e.target.value }))
+      setCommission((prev) => ({ ...prev, [e.target.name]: e.target.value }));
     }
-  }
+  };
 
   //funtion to delete commission
   // const handleDelete = async (commissionID) =>{
@@ -61,12 +61,12 @@ const ViewCommission = () => {
       try {
         const res = await axios.get(
           `http://localhost:8800/commission/${commissionID}`
-        )
-        const retrievedCommission = res.data[0]
+        );
+        const retrievedCommission = res.data[0];
         //format date
         const formattedDate = new Date(retrievedCommission.commissionDeadline)
           .toISOString()
-          .substr(0, 10)
+          .substr(0, 10);
 
         // Update the state with retrieved account data
         setCommission({
@@ -81,99 +81,87 @@ const ViewCommission = () => {
           catcherID: retrievedCommission.catcherID,
           //DatePosted:"",
           //DateCompleted:retrievedCommission.,
-          ContactNo: retrievedCommission.ContactNumber
-        })
+          ContactNo: retrievedCommission.ContactNumber,
+        });
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
-    }
+    };
 
-    fetchCommission()
-  }, [commissionID])
+    fetchCommission();
+  }, [commissionID]);
 
-  const handleClick = async e => {
-    e.preventDefault()
+  const handleClick = async (e) => {
+    e.preventDefault();
     try {
       //account.dateCreated = getCurrentDate();
       await axios.put(
-        'http://localhost:8800/update-commission/' + commissionID,
+        "http://localhost:8800/update-commission/" + commissionID,
         commission
-      )
-      navigate('/commission-list')
+      );
+      navigate("/commission-list");
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
-  console.log(commission)
+  console.log(commission);
 
   return (
     <div>
-            <Navbar />
-    <div className='form errand-cont'>
-      <div className='input-cont'>
-        <div className='errand-inputs'>
-          <h1>View Commission</h1>
-          {/* {imageURL && <img src={imageURL} alt='Commission Type' />} */}
-          {/* commision title */}
-          <div className='input-group'>
-              <label>
-                Commission Title:
-              </label>
+      <Navbar />
+      <div className="form errand-cont">
+        <div className="input-cont">
+          <div className="errand-inputs">
+            <h1>View Commission</h1>
+            {/* {imageURL && <img src={imageURL} alt='Commission Type' />} */}
+            {/* commision title */}
+            <div className="input-group">
+              <label>Commission Title:</label>
               <p> This is title: {commission.comTitle}</p>
+            </div>
+            {/* deadline */}
+            <div className="input-group">
+              <label>Deadline:</label>
+              <p>this is deadline{commission.comDeadline}</p>
+            </div>
+            {/* location */}
+            <div className="input-group">
+              <label>Location</label>
+              <p>this is location{commission.comLocation}</p>
+            </div>
+            {/* commission type */}
+            <div className="input-group">
+              <label htmlFor="">Commission Type:</label>
+              <p>this is commission type{commission.comType}</p>
+            </div>
+            {/* description */}
+            <div className="input-group">
+              <label>Description</label>
+              <p> some description{commission.comDescription}</p>
+            </div>
+            {/* Amount */}
+            <div className="input-group">
+              <label>Amount: ₱</label>
+              <p>1000{commission.comPay}</p>
+            </div>
           </div>
-          {/* deadline */}
-          <div className='input-group'>
-            <label>
-              Deadline:
-            </label>
-            <p>this is deadline{commission.comDeadline}</p>
-          </div>
-          {/* location */}
-          <div className='input-group'>
-            <label>
-                Location
-            </label>
-            <p>this is location{commission.comLocation}</p>
-          </div>
-          {/* commission type */}
-          <div className='input-group'>
-            <label htmlFor=''>
-              Commission Type:
-            </label>
-            <p>this is commission type{commission.comType}</p>
-          </div>
-          {/* description */}
-          <div className='input-group'>
-            <label>
-              Description
-            </label>
-            <p> some description{commission.comDescription}</p>
-          </div>
-          {/* Amount */}
-          <div className='input-group'>
-            <label>
-              Amount: ₱
-            </label>
-            <p>1000{commission.comPay}</p>
-          </div>
-        </div>
           <Map />
         </div>
-      {/* <input type="text" value={commission.ContactNo} onChange={handleChange} placeholder='Contact Number' name='ContactNo'/> */}
-      <div className='btn-container'>
-        <button
-          className='formButton btn'
-          onClick={e => {
-            navigate(`/update-commission/${commission.commissionID}`)
-          }}
-        >
-          APPLY
-        </button>
+        {/* <input type="text" value={commission.ContactNo} onChange={handleChange} placeholder='Contact Number' name='ContactNo'/> */}
+        <div className="btn-container">
+          <button
+            className="formButton btn"
+            onClick={(e) => {
+              navigate(`/update-commission/${commission.commissionID}`);
+            }}
+          >
+            APPLY
+          </button>
+        </div>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default ViewCommission
+export default ViewCommission;
