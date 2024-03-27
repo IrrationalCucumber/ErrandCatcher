57acbb881b40452313606ad03819d797b42e421d
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-03-27 13:29:52 +0800

    changed Cards components used

--- client/src/pages/Services/HomeServices.jsx
+++ client/src/pages/Services/HomeServices.jsx
@@ -1,36 +1,30 @@
-import React,{useState} from 'react'
-import Cards from '../../components/Cards';
-import Navbar from '../../components/Navbar';
-
+import React, { useState } from "react";
+import Cards from "../../components/CatCards";
+import Navbar from "../../components/Navbar";
 
 const HomeServices = () => {
+  const [searchQuery, setSearchQuery] = useState("");
 
-    const [searchQuery, setSearchQuery] = useState('');
-
-    const handleSearchChange = (event) => {
-        setSearchQuery(event.target.value);
-    };
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
   return (
     <>
-    <Navbar/>
-    <h1>Home Services</h1>  
-        <div className="search-bar"> 
-            <input
-                type="text"
-                placeholder="Search..."
-                value={searchQuery}
-                onChange={handleSearchChange}
-            />
-            <button>Search</button>
-        </div>
-        <Cards/> 
-    <div>
-               
-    </div>
-            
-
+      <Navbar />
+      <h1>Home Services</h1>
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchQuery}
+          onChange={handleSearchChange}
+        />
+        <button>Search</button>
+      </div>
+      <Cards />
+      <div></div>
     </>
-  )
-}
+  );
+};
 
-export default HomeServices
+export default HomeServices;
