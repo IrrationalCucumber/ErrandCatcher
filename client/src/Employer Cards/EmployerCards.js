b191b705a992b697e3484119c33776e7890b8569
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-03-30 15:17:35 +0800

    display count

--- client/src/Employer Cards/EmployerCards.js
+++ client/src/Employer Cards/EmployerCards.js
@@ -1,27 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import "./employercard.css"
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./employercard.css";
+import axios from "axios";
 
 const EmployerCard = ({ employer }) => {
   const [counts, setCounts] = useState({
     numPosts: 0,
     numApplicants: 0,
-    numErrands: 0
+    numErrands: 0,
   });
   const [loading, setLoading] = useState(true);
 
+  const location = useLocation();
+  const userID = location.pathname.split("/")[2];
   useEffect(() => {
     // Simulate fetching counts from backend API, be removed
     setTimeout(() => {
-      const simulatedCounts = {
-        numPosts: Math.floor(Math.random() * 10), // Random number of posts
-        numApplicants: Math.floor(Math.random() * 10), // Random number of applicants
-        numErrands: Math.floor(Math.random() * 10) // Random number of errands
+      const fetchCount = async () => {
+        try {
+          const res2 = await axios.get(
+            `http://localhost:8800/post-and-applicant-count/${userID}`
+          );
+          setCounts({
+            numPosts: res2.data[0].postCount,
+            numApplicants: res2.data[0].applicantCount,
+          });
+        } catch (err) {
+          console.log(err);
+        }
       };
-      setCounts(simulatedCounts);
+      fetchCount();
+
+      //   const simulatedCounts = {
+      //     //numPosts: Math.floor(Math.random() * 10), // Random number of posts
+      //     numApplicants: Math.floor(Math.random() * 10), // Random number of applicants
+      //     //numErrands: Math.floor(Math.random() * 10), // Random number of errands
+      //   };
+      //   setCounts(simulatedCounts);
       setLoading(false);
     }, 1000); // Simulate 1 second delay
-  }, []);
+  }, [counts, userID]);
 
   return (
     <div className="employer-card">
@@ -30,17 +48,29 @@ const EmployerCard = ({ employer }) => {
         <p>Loading...</p>
       ) : (
         <div className="count-details">
-          <Link to={`/commissions`} className="count-section posts" style={{color:"black"}}>
+          <Link
+            to={`/commissions`}
+            className="count-section posts"
+            style={{ color: "black" }}
+          >
             <p>{counts.numPosts}</p>
             <h3>Posts</h3>
-          </Link>   
-          <Link to={`/applicants`} className="count-section applicants1" style={{color:"black"}}>
-            <p>{counts.numApplicants}</p>
-            <h3>Applicants</h3>
           </Link>
-          <Link to={`/commissions`} className="count-section errands" style={{color:"black"}}>
-            <p>{counts.numErrands}</p>
+          <Link
+            to={`/applicants/${userID}`}
+            className="count-section applicants1"
+            style={{ color: "black" }}
+          >
+            <h3>Applicants</h3>
+            <p>{counts.numApplicants}</p>
+          </Link>
+          <Link
+            to={`/commissions`}
+            className="count-section errands"
+            style={{ color: "black" }}
+          >
             <h3>Pending</h3>
+            <p>{counts.numErrands}</p>
           </Link>
         </div>
       )}
