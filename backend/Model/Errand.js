917ce4052bf123f0aa2ebb54223fb33a1cf8785c
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-04-04 15:06:26 +0800

    added model for errand

--- backend/Model/Errand.js
+++ backend/Model/Errand.js
@@ -1,3 +1,19 @@
 const db = require("../dbConfig");
 
+const Errands = {
+  //get all errands
+  getAllErrands: (callback) => {
+    db.query("SELECT * FROM commissions");
+  },
+  //get specific errand by id
+  getErrandById: (id, callback) => {
+    db.query(
+      `SELECT * FROM commissions WHERE commissionID = ?`,
+      [id],
+      callback
+    );
+  },
+  //
+};
+
 module.exports = Errand;
