4b83a05ab978c56fa38ef711424a459ef803bf65
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-04-08 12:36:10 +0800

    can save to db

--- client/src/pages/SignUp/Signup.jsx
+++ client/src/pages/SignUp/Signup.jsx
@@ -7,54 +7,55 @@ import RadioInputs from "./RadioInputs";
 
 const Signup = () => {
   const [account, setAccount] = useState({
-    firstName: "",
-    lastName: "",
     regUsername: "",
     regPassword: "",
+    firstName: "",
+    lastName: "",
     regPassword2: "",
+    bday: "",
+    gender: "",
     email: "",
     contactNumber: "",
-    type: "Employer",
+    type: "",
     dateCreated: "",
   });
 
-    //handle state of error message
-    const [employerErrorMessage, setEmployerErrorMessage] = useState("");
-    const [catcherErrorMessage, setCatcherErrorMessage] = useState("");
+  //handle state of error message
+  const [employerErrorMessage, setEmployerErrorMessage] = useState("");
+  const [catcherErrorMessage, setCatcherErrorMessage] = useState("");
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const resetForm = () => {
+    setAccount({
+      regUsername: "",
+      regPassword: "",
+      firstName: "",
+      lastName: "",
+      regPassword2: "",
+      bday: "",
+      gender: "",
+      email: "",
+      contactNumber: "",
+      type: "",
+      dateCreated: "",
+    });
+    setEmployerErrorMessage("");
+    setCatcherErrorMessage("");
+  };
 
-    const [isChecked, setIsChecked] = useState(false);
-
-    const resetForm = () => {
-      setAccount({
-        firstName: "",
-        lastName: "",
-        regUsername: "",
-        regPassword: "",
-        regPassword2: "",
-        email: "",
-        contactNumber: "",
-        dateCreated: "",
-      });
-      setEmployerErrorMessage("");
-      setCatcherErrorMessage("");
-    };
-  
-    const getCurrentDate = () => {
-      const currentDate = new Date();
-      const year = currentDate.getFullYear();
-      const month = String(currentDate.getMonth() + 1).padStart(2, "0");
-      const day = String(currentDate.getDate()).padStart(2, "0");
-      return `${year}-${month}-${day}`;
-    };
-    
+  const getCurrentDate = () => {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+    const day = String(currentDate.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  };
 
   const [selectedType, setSelectedType] = useState("");
   const [selectedOption, setSelectedOption] = useState("");
   const options = [
-    { label: "Employer", value: "employer" },
-    { label: "Catcher", value: "catcher" },
+    { label: "Employer", value: "Employer" },
+    { label: "Catcher", value: "Catcher" },
   ];
 
   const handleOptionChange = (e) => {
@@ -63,95 +64,105 @@ const Signup = () => {
 
   const handleTypeChange = () => {
     setSelectedType(selectedOption);
+    console.log(selectedOption);
+    if (selectedOption == "Catcher") {
+      // Checkbox is checked, store one value
+      setAccount((prev) => ({ ...prev, type: "Catcher" }));
+    } else {
+      // Checkbox is not checked, store another value
+      setAccount((prev) => ({ ...prev, type: "Employer" }));
+    }
   };
 
   const handleReset = () => {
     setSelectedType("");
     setSelectedOption("");
+    resetForm();
   };
 
-  const onSubmit = async (data) => {
-    try {
-      // Your HTTP request code for sign-up goes here
-      console.log(data);
-      // Redirect to login page after successful sign-up
-      window.location.href = "/login";
-    } catch (error) {
-      console.error(error);
-      // Handle error
-    }
-  };
+  // const onSubmit = async (data) => {
+  //   try {
+  //     // Your HTTP request code for sign-up goes here
+  //     console.log(data);
+  //     // Redirect to login page after successful sign-up
+  //     window.location.href = "/sign-in";
+  //   } catch (error) {
+  //     console.error(error);
+  //     // Handle error
+  //   }
+  // };
 
   const handleChange = (e) => {
     // For the 'gender' field, directly set the value without using spread syntax
 
-    if (e.target.name === "type") {
-      if (isChecked) {
-        // Checkbox is checked, store one value
-        setAccount((prev) => ({ ...prev, type: "Catcher" }));
-      } else {
-        // Checkbox is not checked, store another value
-        setAccount((prev) => ({ ...prev, type: "Employer" }));
-      }
+    if (e.target.name === "gender") {
+      setAccount((prev) => ({ ...prev, gender: e.target.value }));
     } else {
       // For other fields, use spread syntax as before
       setAccount((prev) => ({ ...prev, [e.target.name]: e.target.value }));
     }
   };
 
-    //save the data into db
-    const handleClick = async (e) => {
-      //if fileds are empty
-      //error message
-      if (
-        !account.firstName ||
-        !account.lastName ||
-        !account.regPassword ||
-        !account.regPassword2 ||
-        !account.email ||
-        !account.contactNumber ||
-        !account.type
-      ) {
-        if (account.type === "Employer") {
-          setEmployerErrorMessage("Missing fields. Please try again.");
-        } else {
-          setCatcherErrorMessage("Missing fields. Please try again.");
-        }
-        return;
-      } else if (account.regPassword.length < 8) {
-        if (account.type === "Employer") {
-          setEmployerErrorMessage("Password is too short.");
-        } else {
-          setCatcherErrorMessage("Password is too short.");
-        }
-        return;
-      } else if (account.regPassword !== account.regPassword2) {
-        if (account.type === "Employer") {
-          setEmployerErrorMessage("Password does not match.");
-        } else {
-          setCatcherErrorMessage("Password does not match.");
-        }
-        return;
-      }
-          //save to db if no error
+  //save the data into db
+  const handleClick = async (e) => {
+    //if fileds are empty
+    //error message
+    if (
+      !account.firstName ||
+      !account.lastName ||
+      !account.regPassword ||
+      !account.regPassword2 ||
+      !account.email ||
+      !account.contactNumber ||
+      !account.type
+    ) {
+    } else if (account.regPassword.length < 8) {
+      setEmployerErrorMessage("Password is too short.");
+
+      return;
+    } else if (account.regPassword !== account.regPassword2) {
+      setEmployerErrorMessage("Password does not match.");
+      return;
+    }
+    //save to db if no error
     e.preventDefault();
     try {
       account.dateCreated = getCurrentDate();
       await axios.post("http://localhost:8800/signup", account); // new enpoint
+      alert("Success");
       navigate("/sign-in");
     } catch (err) {
       console.log(err);
     }
   };
+  //console.log(account);
 
   return (
-    <div className="SUcontainer" style={{ maxWidth: "950px", margin: "20px auto", padding: "0 15px", textAlign: "center" }}>
-      <h2 style={{ fontFamily: "sans-serif", paddingTop: "20px" }}>Errand Catcher</h2>
-      <form onSubmit={onSubmit}>
+    <div
+      className="SUcontainer"
+      style={{
+        maxWidth: "950px",
+        margin: "20px auto",
+        padding: "0 15px",
+        textAlign: "center",
+      }}
+    >
+      <h2 style={{ fontFamily: "sans-serif", paddingTop: "20px" }}>
+        Errand Catcher
+      </h2>
+      <form>
         <div className="SUrow">
           {!selectedType && (
             <div className="col text-center">
-              <div className="SUcontainer" style={{ maxWidth: "950px", margin: "0 auto", padding: "20px 20px 20px 20px", textAlign: "center" }}>
+              <div
+                className="SUcontainer"
+                style={{
+                  maxWidth: "950px",
+                  margin: "0 auto",
+                  padding: "20px 20px 20px 20px",
+                  textAlign: "center",
+                }}
+              >
                 <h2>Join as an Employer or a Catcher</h2>
               </div>
               <RadioInputs
@@ -160,7 +171,9 @@ const Signup = () => {
                 onChange={handleOptionChange}
               />
               <div>
-                <button onClick={handleTypeChange} disabled={!selectedOption}
+                <button
+                  onClick={handleTypeChange}
+                  disabled={!selectedOption}
                   style={{
                     padding: "10px",
                     width: "200px",
@@ -178,7 +191,10 @@ const Signup = () => {
                   }}
                 >
                   {selectedOption
-                    ? `Join as ${selectedOption.charAt(0).toUpperCase() + selectedOption.slice(1)}`
+                    ? `Join as ${
+                        selectedOption.charAt(0).toUpperCase() +
+                        selectedOption.slice(1)
+                      }`
                     : "Create Account"}
                 </button>
               </div>
@@ -192,110 +208,168 @@ const Signup = () => {
               <div className="text-center mb-4">
                 <h3>Signup to Errand Catcher</h3>
               </div>
-              <form
-                className="mt-4 mb-4"
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  onSubmit();
-                }}
-              >
-                <div className="SUrow" style={{ display: "flex", flexWrap: "wrap", margin: "0 -15px" }}>
+              <form className="mt-4 mb-4">
+                <div
+                  className="SUrow"
+                  style={{
+                    display: "flex",
+                    flexWrap: "wrap",
+                    margin: "0 -15px",
+                  }}
+                >
                   <div className="col">
                     <label className="SUlabel">Username</label>
-                    <input 
-                      type="text" 
-                      placeholder="Username" 
+                    <input
+                      type="text"
+                      placeholder="Username"
                       onChange={handleChange}
                       name="regUsername"
                       value={account.regUsername}
                       autocomplete="off"
-                      required />
+                      required
+                    />
                   </div>
                   <div className="col">
                     <label className="SUlabel">Email Address</label>
-                    <input 
-                      type="email" 
-                      placeholder="Email Address" 
+                    <input
+                      type="email"
+                      placeholder="Email Address"
                       onChange={handleChange}
                       name="email"
                       value={account.email}
-                      required />
+                      required
+                    />
                   </div>
                 </div>
-                <div className="SUrow" style={{ display: "flex", flexWrap: "wrap", margin: "0 -15px" }}>
+                <div
+                  className="SUrow"
+                  style={{
+                    display: "flex",
+                    flexWrap: "wrap",
+                    margin: "0 -15px",
+                  }}
+                >
                   <div className="col">
                     <label className="SUlabel">First Name</label>
-                    <input 
-                      type="text" 
-                      placeholder="First Name" 
+                    <input
+                      type="text"
+                      placeholder="First Name"
                       onChange={handleChange}
                       name="firstName"
                       value={account.firstName}
-                      required />
+                      required
+                    />
                   </div>
                   <div className="col">
                     <label className="SUlabel">Last Name</label>
-                    <input 
-                      type="text" 
-                      placeholder="Last Name" 
+                    <input
+                      type="text"
+                      placeholder="Last Name"
                       onChange={handleChange}
                       name="lastName"
                       value={account.lastName}
-                      required />
+                      required
+                    />
                   </div>
                 </div>
-                <div className="SUrow" style={{ display: "flex", flexWrap: "wrap", margin: "0 -15px" }}>
+                <div
+                  className="SUrow"
+                  style={{
+                    display: "flex",
+                    flexWrap: "wrap",
+                    margin: "0 -15px",
+                  }}
+                >
                   <div className="col">
                     <label className="SUlabel">Birthday</label>
-                    <input 
-                      type="date" 
-                      placeholder="Birthday" 
-                      required />
+                    <input
+                      type="date"
+                      placeholder="Birthday"
+                      required
+                      name="bday"
+                      value={account.bday}
+                      onChange={handleChange}
+                    />
                   </div>
                   <div className="col">
                     <label className="SUlabel">Gender</label>
-                    <select required style={{ width: "100%" }}>
+                    <select
+                      required
+                      style={{ width: "100%" }}
+                      value={account.gender}
+                      onChange={handleChange}
+                      name="gender"
+                    >
                       <option value="">Select Gender</option>
-                      <option value="male">Male</option>
-                      <option value="female">Female</option>
+                      <option value="Male">Male</option>
+                      <option value="Female">Female</option>
                     </select>
                   </div>
                 </div>
-                <div className="SUrow" style={{ display: "flex", flexWrap: "wrap", margin: "0 -15px" }}>
+                <div
+                  className="SUrow"
+                  style={{
+                    display: "flex",
+                    flexWrap: "wrap",
+                    margin: "0 -15px",
+                  }}
+                >
                   <div className="col">
                     <label className="SUlabel">Password</label>
-                    <input 
-                      type="password" 
-                      placeholder="Password" 
+                    <input
+                      type="password"
+                      placeholder="Password"
                       onChange={handleChange}
                       name="regPassword"
                       value={account.regPassword}
                       autoComplete="off"
-                      required />
+                      required
+                    />
                   </div>
                   <div className="col">
                     <label className="SUlabel">Confirm Password</label>
-                    <input 
-                      type="password" 
-                      placeholder="Confirm Password" 
+                    <input
+                      type="password"
+                      placeholder="Confirm Password"
                       onChange={handleChange}
-                      name="regPassword"
+                      name="regPassword2"
                       value={account.regPassword2}
                       autoComplete="off"
-                      required />
+                      required
+                    />
                   </div>
                 </div>
-                <div className="SUrow" style={{ display: "flex", flexWrap: "wrap", margin: "0 -15px" }}>
-                  <div className="col text-center" >
-                    <button type="submit">Sign Up</button>
+                <div
+                  className="SUrow"
+                  style={{
+                    display: "flex",
+                    flexWrap: "wrap",
+                    margin: "0 -15px",
+                  }}
+                >
+                  <div className="col text-center">
+                    <button onClick={handleClick}>Sign Up</button>
                   </div>
                 </div>
                 <div className="m-4" style={{ paddingTop: "20px" }}>
                   Already have an account? <Link to="/sign-in">Sign in</Link>
                 </div>
-                <div className="m-4-return" style={{ paddingTop: "20px", paddingLeft:"20px", border:"none",  position: "absolute", top: 0, left: 0 }}>
-                  <span  onClick={handleReset} style={{cursor:"pointer", fontSize: "20px"}}>
-                    <i class="fa-solid fa-arrow-left"/>
+                <div
+                  className="m-4-return"
+                  style={{
+                    paddingTop: "20px",
+                    paddingLeft: "20px",
+                    border: "none",
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                  }}
+                >
+                  <span
+                    onClick={handleReset}
+                    style={{ cursor: "pointer", fontSize: "20px" }}
+                  >
+                    <i class="fa-solid fa-arrow-left" />
                   </span>
                 </div>
               </form>
