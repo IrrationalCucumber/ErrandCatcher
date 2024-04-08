797e121883bf415b3447c27d4336677342ba0499
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-04-08 19:58:02 +0800

    Application backend initial

---
+++ backend/Controller/ApplicationController.js
@@ -0,0 +1,158 @@
+const Apply = require("../Model/Application");
+
+const applyController = {
+  //retunr applicants of employer
+  getApplicants: (req, res) => {
+    const id = req.params.id;
+    Apply.getApplicants(id, (err, applicants) => {
+      if (err) {
+        console.error("Error fetching Applicants:", err);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+      res.json(applicants);
+    });
+  },
+  //get applications of catcher
+  getApplication: (req, res) => {
+    const id = req.params.id;
+    Apply.getApplication(id, (err, application) => {
+      if (err) {
+        console.error("Error fetching Application:", err);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+      res.json(application);
+    });
+  },
+  //return appID if exist
+  getIfApplied: (req, res) => {
+    const id = req.params.id;
+    const comID = req.params.comID;
+    Apply.getIfApplied(id, comID, (err, applied) => {
+      if (err) {
+        console.error("Error fetching Application:", err);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+      res.json(applied);
+    });
+  },
+  // add an application
+  postApply: (req, res) => {
+    const applyData = req.body;
+    Apply.postApply(applyData, (error) => {
+      if (error) {
+        console.error("Error adding application:", error);
+        res
+          .status(500)
+          .json({ error: "An error occurred while adding new application" });
+        return;
+      }
+      res.status(200).json({ message: "Application added successfully" });
+    });
+  },
+  //Deny applicant
+  putDenyApply: (req, res) => {
+    const status = "Denied";
+    const comID = req.params.comID;
+    const applyID = req.params.applyID;
+    Apply.updateErrandStatus(comID, applyID, status, (error, result) => {
+      if (error) {
+        console.error("Error updating Application:", error);
+        res
+          .status(500)
+          .json({ error: "An error occurred while updating application" });
+        return;
+      }
+      // Check if any rows were affected by the update operation
+      if (result.affectedRows === 0) {
+        res.status(404).json({ error: "Application not found" });
+        return;
+      }
+      res.status(200).json({ message: "Application denied successfully" });
+    });
+  },
+  //Accept applicant
+  putAcceptApply: (req, res) => {
+    const status = "Accepted";
+    const comID = req.params.comID;
+    const applyID = req.params.applyID;
+    Apply.updateErrandStatus(comID, applyID, status, (error, result) => {
+      if (error) {
+        console.error("Error updating Application:", error);
+        res
+          .status(500)
+          .json({ error: "An error occurred while updating application" });
+        return;
+      }
+      // Check if any rows were affected by the update operation
+      if (result.affectedRows === 0) {
+        res.status(404).json({ error: "Application not found" });
+        return;
+      }
+      res.status(200).json({ message: "Application Accepted" });
+    });
+  },
+  //cancel catcher application
+  putCancelApply: (req, res) => {
+    const id = req.params.id;
+    const applyID = req.params.applyID;
+    Apply.putCancelApply(id, applyID, (error, result) => {
+      if (error) {
+        console.error("Error updating Application:", error);
+        res
+          .status(500)
+          .json({ error: "An error occurred while updating application" });
+        return;
+      }
+      // Check if any rows were affected by the update operation
+      if (result.affectedRows === 0) {
+        res.status(404).json({ error: "Application not found" });
+        return;
+      }
+      res.status(200).json({ message: "Application Cancelled" });
+    });
+  },
+  // deny other applicant if one is chosen
+  //employer side
+  putDenyOther: (req, res) => {
+    const comID = req.params.comID;
+    const id = req.params.id;
+    Apply.putDenyOther(comID, id, (error, result) => {
+      if (error) {
+        console.error("Error updating Application:", error);
+        res
+          .status(500)
+          .json({ error: "An error occurred while updating application" });
+        return;
+      }
+      // Check if any rows were affected by the update operation
+      if (result.affectedRows === 0) {
+        res.status(404).json({ error: "Application not found" });
+        return;
+      }
+      res.status(200).json({ message: "Application updated successfully" });
+    });
+  },
+  //delete application
+  // cahtcher side
+  deleteApply: (req, res) => {
+    const id = req.params.id;
+    const comID = req.params.comID;
+    Apply.deleteApply(id, comID, (err, result) => {
+      if (err) {
+        console.error("Error fetching Errand:", err);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+      if (result.affectedRows === 0) {
+        res.status(404).json({ error: "Application not found" });
+        return;
+      }
+      res.status(200).json({ message: "Application deleted successfully" });
+    });
+  },
+};
+
+module.exports = applyController;
---
+++ backend/Route/ApplicationRoute.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const applicationController = require("../Controller/ApplicationController");
+
+const router = express.Router();
+
+router.get("/applicants/:id", applicationController.getApplicants);
+router.get("/your-application/:id", applicationController.getApplication);
+router.get("/get-apply/:id/:comID", applicationController.getIfApplied);
+router.post("/apply", applicationController.postApply);
+router.put("/deny-apply/:comID/:applyID", applicationController.putDenyApply);
+router.put(
+  "/accept-apply/:comID/:applyID",
+  applicationController.putAcceptApply
+);
+router.put("/cancel-apply/:id/:applyID", applicationController.putCancelApply);
+router.put("/deny-other-apply/:comID/:id", applicationController.putDenyOther);
+router.delete("/delete-apply", applicationController.deleteApply);
+module.exports = router;
