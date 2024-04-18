b18c3c033cb630e39336f7b14b97341f7bd4324b
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-04-18 18:17:00 +0800

    save user data to call dynamically

--- client/src/components/AuthContext.js
+++ client/src/components/AuthContext.js
@@ -1,18 +1,29 @@
 // AuthContext.js
-import { createContext, useContext, useState } from "react";
+import React, { createContext, useState, useEffect, useContext } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(() => {
+    // Retrieve user data from local storage on initial load
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
+  useEffect(() => {
+    // Save user data to local storage whenever it changes
+    localStorage.setItem("user", JSON.stringify(user));
+  }, [user]);
 
   const updateUser = (userData) => {
     setUser(userData);
   };
 
   const login = () => setIsLoggedIn(true);
-  const logout = () => setIsLoggedIn(false);
+  const logout = () => {
+    setIsLoggedIn(false);
+    //setUser("");
+  };
 
   return (
     <AuthContext.Provider
