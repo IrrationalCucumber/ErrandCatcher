623f8200b9ad495ff5a0935fa7f2b6763809ee7c
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-04-20 12:26:01 +0800

    errand markers based on user type

--- client/src/pages/employer/EmployerMap.jsx
+++ client/src/pages/employer/EmployerMap.jsx
@@ -1,11 +1,58 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import Map from "../../components/Map";
-import Navbar from "../../components/Navbar/Navbar";
-import { useLocation } from "react-router-dom";
+//import Navbar from "../../components/Navbar/Navbar";
+import { useAuth } from "../../components/AuthContext";
+import maplibregl from "maplibre-gl";
 
 function CommissionMap() {
-  const location = useLocation();
-  const userID = location.pathname.split("/")[2];
+  const { user } = useAuth();
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng] = useState(123.8854);
+  const [lat] = useState(10.3157);
+  const [zoom] = useState(10);
+  const [API_KEY] = useState("ZQyqv6eWtI6zNE29SPDd");
+
+  const fetchCommissions = async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:8800/your-commission/" + user.userID
+      );
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  };
+
+  useEffect(() => {
+    if (map.current) return;
+
+    map.current = new maplibregl.Map({
+      container: mapContainer.current,
+      style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${API_KEY}`,
+      center: [lng, lat],
+      zoom: zoom,
+    });
+
+    map.current.addControl(new maplibregl.NavigationControl(), "top-right");
+
+    // Fetch commission data and add markers
+    fetchCommissions().then((commissions) => {
+      commissions.forEach((commission) => {
+        const marker = new maplibregl.Marker({ color: "#FF0000" }) // Red marker for commissions
+          .setLngLat([commission.commissionLong, commission.commissionLat])
+          .setPopup(
+            new maplibregl.Popup().setHTML(
+              `<h3>${commission.commissionTitle}</h3><p>${commission.commissionDesc}</p>`
+            )
+          )
+          .addTo(map.current);
+      });
+    });
+    map.current.addControl(new maplibregl.GeolocateControl(), "top-right"); // button to get location
+  }, [API_KEY, lat, lng, zoom]);
   return (
     <div>
       {/* <Navbar
@@ -18,7 +65,7 @@ function CommissionMap() {
         page4="MAP"
         map={`/e-map/${userID}`}
       /> */}
-      <Map />
+      <Map mapContainer={mapContainer} />
     </div>
   );
 }
