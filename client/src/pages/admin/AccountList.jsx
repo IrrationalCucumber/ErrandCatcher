b323ac60a5c1a0db08bf3a8da3bc63a1c3d64d73
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-04-21 14:04:15 +0800

    view profile modal

--- client/src/pages/admin/AccountList.jsx
+++ client/src/pages/admin/AccountList.jsx
@@ -1,12 +1,21 @@
 //updated
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import NavBar from "../../components/Navbar/Navbar";
 import "./accountlist.css";
 import Pagination from "../../components/Pagination";
 import Table from "../../components/Table";
 import { useAuth } from "../../components/AuthContext";
+// ui components
+import Dropdown from "@mui/joy/Dropdown";
+import Menu from "@mui/joy/Menu";
+import MenuButton from "@mui/joy/MenuButton";
+import MenuItem from "@mui/joy/MenuItem";
+import Modal from "@mui/joy/Modal";
+import ModalClose from "@mui/joy/ModalClose";
+import ModalDialog from "@mui/joy/ModalDialog";
+import DialogTitle from "@mui/joy/DialogTitle";
+import DialogContent from "@mui/joy/DialogContent";
 
 const AccountList = () => {
   const [accounts, setAccounts] = useState([]);
@@ -17,14 +26,18 @@ const AccountList = () => {
   });
   // const { user } = useAuth();
   // const userID = user.userID;
-
+  const navigate = useNavigate();
+  const [layout, setLayout] = useState(undefined);
+  const [currentId, setCurrentId] = useState(null);
+  const handleOpenModal = (id) => {
+    setLayout("fullscreen");
+    setCurrentId(id); // Set the ID in state
+  };
   //pagination --Ash
   const [currentPage, setCurrentPage] = useState(1);
-
   //Pagination --Ash
   //display data per page
   const [itemsPerPage] = useState(10);
-
   //useEffect to handle error
   useEffect(() => {
     const fetchAllAccount = async () => {
@@ -40,6 +53,118 @@ const AccountList = () => {
     };
     fetchAllAccount();
   }, []);
+  //view user details
+  const [account, setAccount] = useState({
+    username: "",
+    password: "",
+    lname: "",
+    fname: "",
+    gender: "",
+    email: "",
+    contact: "",
+    age: "",
+    bday: "",
+    address: "",
+    desc: "",
+    profileImage: "",
+    dateC: "",
+    status: "",
+  });
+  //pre-fill the fields
+  useEffect(() => {
+    const fetchAccount = async () => {
+      try {
+        const res = await axios.get(`http://localhost:8800/user/${currentId}`);
+        const retrievedAccount = res.data[0];
+        // Update the state with retrieved account data
+        setAccount({
+          username: retrievedAccount.username,
+          password: retrievedAccount.password,
+          lname: retrievedAccount.userLastname,
+          fname: retrievedAccount.userFirstname,
+          gender: retrievedAccount.userGender,
+          email: retrievedAccount.userEmail,
+          contact: retrievedAccount.userContactNum,
+          age: retrievedAccount.userAge,
+          bday: formattedDate(retrievedAccount.userBirthday),
+          address: retrievedAccount.userAddress,
+          desc: retrievedAccount.userDesc,
+          profileImage: retrievedAccount.profileImage,
+          dateC: formattedDate(retrievedAccount.dateCreated),
+          status: retrievedAccount.accountStatus,
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAccount();
+  }, [currentId]);
+  //get the rating of the user
+  const [rating, setRating] = useState("0");
+  useEffect(() => {
+    const fetchRating = async () => {
+      try {
+        const res = await axios.get(
+          `http://localhost:8800/user-rating/${currentId}`
+        );
+        if (!!res.data) {
+          setRating(res.data[0].c);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchRating();
+  }, [currentId]);
+  //user trans
+  // const [count, setCount] = useState({
+  //   done: "",
+  //   expired: "",
+  //   cancel: "",
+  // });
+  // useEffect(() => {
+  //   const fetchTransCount = async () => {
+  //     try {
+  //       const res = await axios.get(
+  //         `http://localhost:8800/trans-count/${currentId}`
+  //       );
+  //       //console.log(res.data[0].expired);
+  //       if (!!res.data) {
+  //         setCount({
+  //           done: res.data[0].done,
+  //           expired: res.data[0].expired,
+  //           cancel: res.data[0].cancel,
+  //         });
+  //       }
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
+  //   fetchTransCount();
+  // }, [currentId]);
+  //FOR NOTIFICATION
+  //set variables for notification
+  const [notif, setNotif] = useState({
+    userID: "", //this is the employer/ userID of the commission
+    notificationType: "", //notif description
+    notifDesc: "", //contents of the notif
+    notifDate: "", //time and date notif is added
+  });
+  //get current time and date for notif
+  const getTimeAndDate = () => {
+    const currentDate = new Date();
+    // Get the date components
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, "0"); // Months are zero-based
+    const day = String(currentDate.getDate()).padStart(2, "0");
+    // Get the time components
+    const hours = String(currentDate.getHours()).padStart(2, "0");
+    const minutes = String(currentDate.getMinutes()).padStart(2, "0");
+    const seconds = String(currentDate.getSeconds()).padStart(2, "0");
+
+    // Create a string representing the current date and time
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  };
 
   const handleChange = (e) => {
     // For the 'gender' field, directly set the value without using spread syntax
@@ -109,6 +234,7 @@ const AccountList = () => {
     "Type",
     "Date Created",
     "Status",
+    "",
   ];
   const accountData = currentAccounts.map((account) => [
     account.userID,
@@ -118,25 +244,83 @@ const AccountList = () => {
     account.accountType,
     formattedDate(account.dateCreated),
     account.accountStatus,
+    <Dropdown>
+      <MenuButton>ACTIONS</MenuButton>
+      <Menu>
+        <MenuItem
+          onClick={() => {
+            handleOpenModal(account.userID);
+          }}
+        >
+          View
+        </MenuItem>
+        <MenuItem onClick={() => onSuspend(account.userID)}>Suspend</MenuItem>
+        <MenuItem onClick={() => onDeactivate(account.userID)}>
+          Deactivate
+        </MenuItem>
+        <MenuItem onClick={() => onReactivate(account.userID)}>
+          Reactivate
+        </MenuItem>
+      </Menu>
+    </Dropdown>,
   ]);
-  console.log(searchTerm);
+  //console.log(searchTerm);
+  /**
+   * ACTIONS OF ADMIN
+   */
+  //view profile
+  const onView = (id) => {
+    navigate(`/profile/user/${id}`);
+  };
+  //suspend user
+  const onSuspend = async (id) => {
+    try {
+      const status = "Suspended";
+      await axios.put(`http://localhost:8800/change-status/${id}/${status}`);
+      //console.log("Request verified:", request);
+      //setIsButtonClicked(true);
+      //add a notification to the request user
+      notif.notifDesc = "Your account has been suspended";
+      notif.userID = id;
+      notif.notificationType = "Suspension";
+      notif.notifDate = getTimeAndDate();
+      await axios.post("http://localhost:8800/notify", notif);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  //Reactivate user account
+  const onReactivate = async (id) => {
+    try {
+      const status = "Verified";
+      await axios.put(`http://localhost:8800/change-status/${id}/${status}`);
+      //add a notification to the request user
+      notif.notifDesc = "Your account has been reactivated";
+      notif.userID = id;
+      notif.notificationType = "Account Reactivation";
+      notif.notifDate = getTimeAndDate();
+      await axios.post("http://localhost:8800/notify", notif);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const onDeactivate = async (id) => {
+    try {
+      const status = "Deactivated";
+      await axios.put(`http://localhost:8800/change-status/${id}/${status}`);
+      //add a notification to the request user
+      notif.notifDesc = "Your account has been deactivated";
+      notif.userID = id;
+      notif.notificationType = "Account Deactivated";
+      notif.notifDate = getTimeAndDate();
+      await axios.post("http://localhost:8800/notify", notif);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-  //list need to be in a column
-  //need filter
   return (
     <div>
-      {/* <NavBar
-     page1="REQUESTS"
-        one={`/request/${userID}`}
-        // {`admin-home/${userID}`}
-        page2="ACCOUNTS"
-        commissionList={`/accounts/${userID}`}
-        page3="ERRANDS"
-        applicants={`/commission-list/${userID}`}
-        page4="MAP"
-        map={`/map/${userID}`}
-      /> */}
-
       <h1
         className="header"
         style={{
@@ -197,7 +381,6 @@ const AccountList = () => {
           name="status"
           onChange={handleChange}
           value={searchTerm.status}
-          id=""
           style={{
             padding: "8px",
             fontSize: "12px",
@@ -217,13 +400,66 @@ const AccountList = () => {
       <div className="accounts">
         {/*table*/}
         <Table headers={headers} data={accountData} />
-
         <Pagination
           itemsPerPage={itemsPerPage}
           totalItems={accounts.length}
           paginate={paginate}
         />
       </div>
+      <Modal open={!!layout} onClose={() => setLayout(undefined)}>
+        <ModalDialog layout={layout}>
+          <ModalClose />
+          <DialogTitle>{account.username.toUpperCase()} PROFILE</DialogTitle>
+          <DialogContent>
+            <>
+              <div>
+                <img
+                  src={
+                    `http://localhost:8800/images/profile/` +
+                    account.profileImage
+                  }
+                  alt="Profile"
+                  width={150}
+                  length={150}
+                />
+                <h4>USER: {currentId}</h4>
+                <h5>
+                  <b>USERNAME: </b>
+                  {account.username}
+                  <br />
+                  <b>FULL NAME: </b>
+                  {account.fname} {account.lname}
+                  <br />
+                  <b>EMAIL: </b>
+                  {account.email}
+                  <br />
+                  <b>GENDER: </b>
+                  {account.gender} <br />
+                  <b>CONTACT #: </b>
+                  {account.contact} <br />
+                  <b>AGE: </b>
+                  {account.age} <br />
+                  <b>BIRTHDAY: </b>
+                  {account.bday} <br />
+                  <b>ADDRESS: </b>
+                  {account.address} <br />
+                  <b>DESCRIPTION: </b>
+                  {account.desc} <br />
+                  <b>DATE CREATED: </b>
+                  {account.dateC} <br />
+                  <b>STATUS: </b>
+                  {account.status} <br />
+                  <b>RATING: </b>
+                  {rating} <br />
+                  {/* <b>ERRAND DONE: </b> {count.done} <br />
+                  <b>ERRAND EXPIRED: </b> {count.expired} <br />
+                  <b>ERRAND CANCELLED: </b> {count.cancel} <br /> */}
+                </h5>
+              </div>
+            </>
+          </DialogContent>
+        </ModalDialog>
+      </Modal>
       <Link to="/profile/add" style={{ textDecoration: "none" }}>
         <button
           style={{
