2100eb7ef6d876e02e0d92597a6270dd1ba31681
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-05-02 14:15:13 +0800

    carousel component

---
+++ client/src/components/Carousel/Carousel.js
@@ -0,0 +1,71 @@
+import React, { useCallback, useEffect, useState } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
+import { NextButton, PrevButton, usePrevNextButtons } from "./ArrowButtons";
+
+const EmblaCarousel = (props) => {
+  const { slides, options } = props;
+  const [emblaRef, emblaApi] = useEmblaCarousel(options, [
+    Autoplay({ playOnInit: true, delay: 3000 }),
+  ]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick,
+  } = usePrevNextButtons(emblaApi);
+
+  const onButtonAutoplayClick = useCallback(
+    (callback) => {
+      const autoplay = emblaApi?.plugins()?.autoplay;
+      if (!autoplay) return;
+
+      callback();
+    },
+    [emblaApi]
+  );
+
+  useEffect(() => {
+    const autoplay = emblaApi?.plugins()?.autoplay;
+    if (!autoplay) return;
+
+    setIsPlaying(autoplay.isPlaying());
+    emblaApi
+      .on("autoplay:play", () => setIsPlaying(true))
+      .on("autoplay:stop", () => setIsPlaying(false))
+      .on("reInit", () => setIsPlaying(autoplay.isPlaying()));
+  }, [emblaApi]);
+
+  return (
+    <div className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {slides.map((index) => (
+            <div className="embla__slide" key={index}>
+              <div className="embla__slide__number">
+                <span>{index + 1}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="embla__controls">
+        <div className="embla__buttons">
+          <PrevButton
+            onClick={() => onButtonAutoplayClick(onPrevButtonClick)}
+            disabled={prevBtnDisabled}
+          />
+          <NextButton
+            onClick={() => onButtonAutoplayClick(onNextButtonClick)}
+            disabled={nextBtnDisabled}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmblaCarousel;
