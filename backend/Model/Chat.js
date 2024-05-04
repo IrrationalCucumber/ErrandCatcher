538c777a7966f8839704788743ac48d107eeadf2
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-05-04 16:01:02 +0800

    updated variables

--- backend/Model/Chat.js
+++ backend/Model/Chat.js
@@ -1,4 +1,4 @@
-const db = require("../dbConfig");
+const db = require("postgres://postgres.jxmlevrskooknnplyvbh:AnxiousCucumber99@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres");
 
 const Chat = {
   //get the convo of user
@@ -7,7 +7,7 @@ const Chat = {
   getChatById: (id, userID, callback) => {
     db.query(
       `SELECT ch.*, co.* 
-      FROM chat_user ch
+      FROM errand_chat ch
       JOIN chat_conversation co ON ch.chatID = co.convoID
       WHERE chatID = ? AND (chatEmpID = ? OR chatCatchID = ?)`,
       [id, userID, userID],
@@ -20,7 +20,7 @@ const Chat = {
     const { empID, catchID } = chat;
     values = [empID, catchID];
     db.query(
-      "INSERT INTO chat_user (`chatEmpID`, `chatCatchID`, `dateCreated`) VALUES (?, ?, NOW())",
+      "INSERT INTO chat_user (`chat_EmpID`, `chat_CatchID`, `dateCreated`) VALUES (?, ?, NOW())",
       [values],
       callback
     );
