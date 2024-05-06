c3dab1f92c9527b0699044d839c61939dc6e3cc6
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-05-06 14:16:18 +0800

    chat & convo page

--- client/src/pages/Chat/ChatPage.jsx
+++ client/src/pages/Chat/ChatPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Conversation from "../../components/Chat/Conversation";
+import Chats from "../../components/Chat/Chats";
 
 function ChatPage() {
   return (
     <div>
+      <Chats />
       <Conversation />
     </div>
   );
