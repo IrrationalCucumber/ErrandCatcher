6bec84f4173c0b989ce134d9a1338ac87ba90328
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-05-06 14:15:05 +0800

    applied ui design

--- client/src/components/Chat/Conversation.js
+++ client/src/components/Chat/Conversation.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useAuth } from "../AuthContext";
 import axios from "axios";
 import { useLocation } from "react-router-dom";
-import { Button, Input } from "@mui/joy";
+import { Button, Chip, Input, Stack, Typography } from "@mui/joy";
 //icons
 import SendIcon from "@mui/icons-material/Send";
 import MessageIcon from "@mui/icons-material/Message";
@@ -22,9 +22,7 @@ function Conversation() {
   //get chat info
   const fetchChat = async () => {
     try {
-      const res = await axios.get(
-        `http://localhost:8800/chat/${chatID}/${user.userID}`
-      );
+      const res = await axios.get(`http://localhost:8800/chat/${chatID}`);
       if (res.data[0].chat_EmpID === user.userID) {
         setChatInfo({
           chatID: res.data[0].chatID,
@@ -87,28 +85,63 @@ function Conversation() {
   // console.log(chatInfo);
   return (
     <div>
-      <h1>
-        {chatInfo.chatID} <b>{username}</b>
-      </h1>
+      <nav style={{ backgroundColor: "skyblue" }}>
+        <Typography level="h1">{username}</Typography>
+      </nav>
+
       {convos.map((convo) => (
         <div key={convo.convoID}>
           {convo.senderID === user.userID && (
-            <p
+            <Stack
+              direction="column"
+              justifyContent="center"
+              alignItems="flex-end"
+              spacing={1}
               style={{
-                backgroundColor: "skyblue",
+                margin: "10px",
+                position: "flex",
               }}
             >
-              {user.username}: {convo.message}
-            </p>
+              <Typography
+                level="body-md"
+                style={{
+                  backgroundColor: "skyblue",
+                  padding: "15px",
+                  margin: "10px",
+                  borderRadius: "10px 10px 0px 10px",
+                  width: "20%",
+                }}
+              >
+                {convo.message}
+              </Typography>
+            </Stack>
           )}
           {convo.senderID !== user.userID && (
-            <p
+            <div
               style={{
-                backgroundColor: "gray",
+                padding: "10px",
+                margin: "10px",
+                position: "flex",
               }}
             >
-              {username}: {convo.message}
-            </p>
+              <Chip color="neutral" size="lg" variant="soft">
+                {username}
+              </Chip>
+              <Typography
+                level="body-md"
+                color="plain"
+                variant="outlined"
+                style={{
+                  backgroundColor: "gray",
+                  padding: "10px",
+                  margin: "10px 20px",
+                  borderRadius: "0px 10px 10px 10px",
+                  width: "20%",
+                }}
+              >
+                {convo.message}
+              </Typography>
+            </div>
           )}
           {/* <i>{new Date(convo.dateSent).toLocaleDateString()}</i> */}
         </div>
