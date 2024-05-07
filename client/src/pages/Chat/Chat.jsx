adad768570adc3f8cc7b4418ca198b7991e2bfb3
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-05-07 19:59:31 +0800

    added navbar

--- client/src/pages/Chat/Chat.jsx
+++ client/src/pages/Chat/Chat.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../../components/Footer";
+import Navbar from "../../components/Navbar/Navbar";
 
 function Chat() {
   return (
     <div>
+      <Navbar />
       <Outlet />
       <Footer />
     </div>
