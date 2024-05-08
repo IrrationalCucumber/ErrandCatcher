d1533dbbc5e86fd9492528dc11462a195685f3a9
Mamonde02 <valerosojohnraymund@gmail.com>
2024-05-08 23:34:43 +0800

    trigger button step

--- client/src/pages/profile/Verification.jsx
+++ client/src/pages/profile/Verification.jsx
@@ -6,86 +6,23 @@ import Navbar from "../../components/Navbar/Navbar";
 import axios from "axios";
 import { useAuth } from "../../components/AuthContext";
 
-function Verification() {
+// Step 1: Component: Basic Info
+const Step1 = ({ onNext }) => {
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onNext(); // Move to the next step
+  };
+
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [age, setAge] = useState("");
-  const [currentStep, setCurrentStep] = useState(0);
-  const { user } = useAuth();
-  const userID = user.userID;
-
-  const handleNext = () => {
-    if (
-      firstName.trim() === "" ||
-      lastName.trim() === "" ||
-      age.trim() === ""
-    ) {
-      alert("Please fill in all the required fields.");
-    } else if (parseInt(age) < 18) {
-      alert("You must be at least 18 years old to proceed.");
-    } else {
-      setCurrentStep(currentStep + 1);
-    }
-  };
 
   const handleAgeChange = (event) => {
     setAge(event.target.value);
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  };
-
-  const [image1, setImage1] = useState("");
-  const [image2, setImage2] = useState("");
-
-  function handleImage(e) {
-    // const selectedFiles = e.target.files;
-    // if (selectedFiles.length > 2) {
-    //   // If more than two files are selected, alert the user
-    //   alert("You can only upload up to two images.");
-    //   // Clear the file input
-    //   e.target.value = null;
-    //   return;
-    // }
-    // // Set the images state with the selected files
-    // setImages(selectedFiles);
-    if (e.target.name === "image1") {
-      setImage1(e.target.files[0]);
-    } else if (e.target.name === "image2") {
-      setImage2(e.target.files[0]);
-    }
-  }
-  // console.log(image1);
-  // console.log(image2);
-  const handleUpload = async (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    // for (const fileKey in files) {
-    //   if (files.hasOwnProperty(fileKey)) {
-    //     const fileArray = files[fileKey];
-    //     if (Array.isArray(fileArray)) {
-    //       fileArray.forEach((file) => {
-    //         formData.append("files", file);
-    //       });
-    //     } else {
-    //       formData.append("files", fileArray);
-    //     }
-    //   }
-    // }
-    //append them into one
-    formData.append("image1", image1);
-    formData.append("image2", image2);
-    console.log(formData);
-    await axios
-      .post(`http://localhost:8800/upload/${userID}`, formData)
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
-    setCurrentStep(currentStep + 1);
-  };
-  //console.log();
-
-  const list = [
+  return (
     <div className="step">
       <h1>Basic Information</h1>
       {/*step 1 for input logic part is lacking where user input auto fill up */}
@@ -157,28 +94,86 @@ function Verification() {
               required
             ></input>
           </div>
-
           <div>
-            <button
-              type="submit"
-              onClick={handleNext}
-              style={{
-                margin: "20px",
-                padding: "10px",
-                width: "100px",
-                borderRadius: "5px",
-              }}
-            >
-              Next
-            </button>
+          <button type="submit"
+            style={{
+              margin: "20px",
+              padding: "10px",
+              width: "100px",
+              borderRadius: "5px",
+            }}
+          >Next</button>
           </div>
         </div>
       </form>
-    </div>,
-    <div className="step">
+    </div>
+  );
+};
+
+// Step 2: Upload file
+const Step2 = ({ onPrev, onNext }) => {
+  const { user } = useAuth();
+  const userID = user.userID;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onNext();
+  };
+
+  const [image1, setImage1] = useState("");
+  const [image2, setImage2] = useState("");
+
+  function handleImage(e) {
+    // const selectedFiles = e.target.files;
+    // if (selectedFiles.length > 2) {
+    //   // If more than two files are selected, alert the user
+    //   alert("You can only upload up to two images.");
+    //   // Clear the file input
+    //   e.target.value = null;
+    //   return;
+    // }
+    // // Set the images state with the selected files
+    // setImages(selectedFiles);
+    if (e.target.name === "image1") {
+      setImage1(e.target.files[0]);
+    } else if (e.target.name === "image2") {
+      setImage2(e.target.files[0]);
+    }
+  }
+  // console.log(image1);
+  // console.log(image2);
+  const handleUpload = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    // for (const fileKey in files) {
+    //   if (files.hasOwnProperty(fileKey)) {
+    //     const fileArray = files[fileKey];
+    //     if (Array.isArray(fileArray)) {
+    //       fileArray.forEach((file) => {
+    //         formData.append("files", file);
+    //       });
+    //     } else {
+    //       formData.append("files", fileArray);
+    //     }
+    //   }
+    // }
+    //append them into one
+    formData.append("image1", image1);
+    formData.append("image2", image2);
+    console.log(formData);
+    await axios
+      .post(`http://localhost:8800/upload/${userID}`, formData)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+    // setCurrentStep(currentStep + 1);
+  };
+  
+  return (
+    <div>
+      <div className="step">
       <h1>Upload Image</h1>
       <div className="form-group1">
-        <form className="form-container">
+        <form className="form-container" onSubmit={handleSubmit}>
           <div className="input-rows2">
             <label
               className="label2"
@@ -237,7 +232,8 @@ function Verification() {
           <button onClick={handleUpload}>SUBMIT</button>
           <div className="input-rows" style={{ justifyContent: "center" }}>
             <button
-              onClick={() => setCurrentStep(currentStep - 1)}
+              type="button"
+              onClick={onPrev}
               style={{
                 margin: "20px",
                 padding: "10px",
@@ -248,7 +244,7 @@ function Verification() {
               Prev
             </button>
             <button
-              onClick={() => setCurrentStep(currentStep + 1)}
+              type="submit"
               style={{
                 margin: "20px",
                 padding: "10px",
@@ -261,8 +257,14 @@ function Verification() {
           </div>
         </form>
       </div>
-    </div>,
+    </div>
+    </div>
+  );
+};
 
+// Step 3: 
+const Step3 = ({ onPrev }) => {
+  return (
     <div className="step">
       <form className="form-container">
         <h1>Sending Verifification to the Admin!</h1>
@@ -272,14 +274,18 @@ function Verification() {
           Explore the services in Errand Catcher
         </p>
       </form>
-    </div>,
-  ];
-
-  return (
-    <div className="App">
-      <Stepper list={list} currentStep={currentStep} />
     </div>
   );
-}
+};
+
+const Verification = () => {
+  const steps = [<Step1 />, <Step2 />, <Step3 />];
+
+  return (
+    <div>
+      <Stepper list={steps} />
+    </div>
+  );
+};
 
 export default Verification;
