84dbcaa2c5c09e87b1293b03a3f4aa16538cf2b0
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-05-15 11:54:22 +0800

    combine filter and map component

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -2,11 +2,15 @@ import React, { useRef, useEffect, useState } from "react";
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import "./css/map.css";
+import Filter from "./Filter";
 
 export default function Map(props) {
   return (
-    <div className="map-wrap">
-      <div ref={props.mapContainer} className="map" />
-    </div>
+    <>
+      <div className="map-wrap">
+        <Filter onFilterChange={props.change} />
+        <div ref={props.mapContainer} className="map" />
+      </div>
+    </>
   );
 }
