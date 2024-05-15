a9f36125f9f657a0cf0d3708215eb069bb6cc6e5
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-05-15 13:32:18 +0800

    filter initial design

--- client/src/components/Map/Filter.jsx
+++ client/src/components/Map/Filter.jsx
@@ -11,17 +11,21 @@ function Filter({ onFilterChange }) {
   };
 
   return (
-    <div>
-      <header>
-        {/* <Input placeholder="Enter title here.." name="term" /> */}
-        <select name="type" onChange={handleChange} value={filter}>
-          <option value="">TYPE</option>
-          <option value="HomeService">Home Service</option>
-          <option value="Delivery">Delivery</option>
-          <option value="Transportation">Transportations</option>
-        </select>
-        {/* <button onClick={SetFilters}>SEARCH</button> */}
-      </header>
+    <div className="map__filter">
+      {/* <Input placeholder="Enter title here.." name="term" /> */}
+      <h3 className="map__filter__text">FILTER</h3>
+      <select
+        className="map__filter__type"
+        name="type"
+        onChange={handleChange}
+        value={filter}
+      >
+        <option value="">TYPE</option>
+        <option value="HomeService">Home Service</option>
+        <option value="Delivery">Delivery</option>
+        <option value="Transportation">Transportations</option>
+      </select>
+      {/* <button onClick={SetFilters}>SEARCH</button> */}
     </div>
   );
 }
