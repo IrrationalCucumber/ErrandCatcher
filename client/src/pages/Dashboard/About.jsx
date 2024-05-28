5c1de9c81fd70dc7e900be019b9aeb47f50db5fb
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-05-28 12:50:33 +0800

    add comments

--- client/src/pages/Dashboard/About.jsx
+++ client/src/pages/Dashboard/About.jsx
@@ -1,3 +1,4 @@
+// rmove comments if to-do is done
 import React from "react";
 
 function About() {
@@ -6,38 +7,51 @@ function About() {
       <h1>About</h1>
       <section>
         <h2>Unusual Beetles</h2>
-        <p></p>
+        {/* Add details about group */}
+        <p>Details</p>
       </section>
       <section>
         <h2>Team</h2>
         <div>
+          {/* profile pic of member */}
           <img src="" alt="" />
           <h3>Ashley Belle Carcuevas</h3>
+          {/* Add details about individual */}
           <p>Details</p>
         </div>
         <div>
+          {/* profile pic of member */}
           <img src="" alt="" />
           <h3>Lorry Mae Halina</h3>
+          {/* Add details about individual */}
           <p>Details</p>
         </div>
         <div>
+          {/* profile pic of member */}
           <img src="" alt="" />
           <h3>Angel Diane Ocier</h3>
+          {/* Add details about individual */}
           <p>Details</p>
         </div>
         <div>
+          {/* profile pic of member */}
           <img src="" alt="" />
           <h3>Trisha Mae Sasing</h3>
+          {/* Add details about individual */}
           <p>Details</p>
         </div>
         <div>
+          {/* profile pic of member */}
           <img src="" alt="" />
           <h3>Adrean Paul Sorono</h3>
+          {/* Add details about individual */}
           <p>Details</p>
         </div>
         <div>
+          {/* profile pic of member */}
           <img src="" alt="" />
           <h3>Raymund Valeroso</h3>
+          {/* Add details about individual */}
           <p>Details</p>
         </div>
       </section>
