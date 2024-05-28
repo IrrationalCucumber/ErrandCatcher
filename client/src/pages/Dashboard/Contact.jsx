8186900ad7348da5610f8b8942b5f9e3fea9e652
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-05-28 12:54:12 +0800

    initial contents of contact page

--- client/src/pages/Dashboard/Contact.jsx
+++ client/src/pages/Dashboard/Contact.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <>
+      <h1>Contact Us</h1>
+      <section>
+        <div>
+          <h3>
+            <b>Email:</b> sample@Email.com
+          </h3>
+        </div>
+        <div>
+          <h3>
+            <b>Tel. #:</b> 911
+          </h3>
+        </div>
+        <div>
+          <h3>
+            <b>Facebook:</b> facebook
+          </h3>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Contact;
