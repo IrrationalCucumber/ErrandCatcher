eac00bb2b1f2c1e02a1407940f87eefbdf6dcd47
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-05-28 12:46:50 +0800

    initial contents in about

--- client/src/pages/Dashboard/About.jsx
+++ client/src/pages/Dashboard/About.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <h1>About</h1>
+      <section>
+        <h2>Unusual Beetles</h2>
+        <p></p>
+      </section>
+      <section>
+        <h2>Team</h2>
+        <div>
+          <img src="" alt="" />
+          <h3>Ashley Belle Carcuevas</h3>
+          <p>Details</p>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <h3>Lorry Mae Halina</h3>
+          <p>Details</p>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <h3>Angel Diane Ocier</h3>
+          <p>Details</p>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <h3>Trisha Mae Sasing</h3>
+          <p>Details</p>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <h3>Adrean Paul Sorono</h3>
+          <p>Details</p>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <h3>Raymund Valeroso</h3>
+          <p>Details</p>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default About;
