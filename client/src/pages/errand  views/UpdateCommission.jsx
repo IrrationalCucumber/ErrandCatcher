6167ba83f31fe4e0b44d06fdf4b34795cb4c9951
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-05-31 12:35:46 +0800

    remove feedback func

--- client/src/pages/errand  views/UpdateCommission.jsx
+++ client/src/pages/errand  views/UpdateCommission.jsx
@@ -1,12 +1,9 @@
 import axios from "axios";
-import React, { useRef, useEffect, useState } from "react";
-import maplibregl from "maplibre-gl";
-import { useNavigate, Link, useLocation } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 //import "../style.css";
 import ErrandInputs from "../../components/ErrandInputs";
-import Navbar from "../../components/Navbar/NavBarPage";
 import "./Commission.css"; // Import your CSS file
-import Modals from "../../components/Modals";
 import { useAuth } from "../../components/AuthContext";
 import Map from "../../components/Map/ViewMapBox";
 import { MapLibre } from "../../components/Map/Map";
@@ -25,84 +22,14 @@ const UpdateCommission = () => {
     ContactNo: "",
     comLong: "",
     comLat: "",
-    destLng: 0,
-    destLat: 0,
+    destLng: "",
+    destLat: "",
     method: "",
   });
 
-  const [feedback, setFeedback] = useState({
-    catcherID: "",
-    commissionID: "",
-    feedbackComment: "",
-    feedbackCount: 0,
-    feebackDate: "",
-    employerID: "",
-  });
-
-  const [successMsg, setSuccessMsg] = useState("");
-  const [error, setError] = useState(null);
   const [distance, setDistance] = useState(0);
   const accessToken =
     "pk.eyJ1Ijoiam9pbmVyIiwiYSI6ImNsdmNjbnF4NjBoajQycWxoaHV5b2M1NzIifQ.Z7Pi_LfWyuc7a_z01zKMFg";
-
-  const getCurrentDate = () => {
-    const currentDate = new Date();
-    const year = currentDate.getFullYear();
-    const month = String(currentDate.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed, so add 1
-    const day = String(currentDate.getDate()).padStart(2, "0");
-    return `${year}-${month}-${day}`;
-  };
-  // modal state //
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [inputValue, setInputValue] = useState({ feedbacks: "" });
-  // rating //
-  const [rating, setRating] = useState(0);
-
-  // modal handle //
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
-  // update changes
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setInputValue({ ...inputValue, [name]: value });
-    setFeedback({ ...feedback, feedbackCount: value });
-    setFeedback({ ...feedback, feedbackComment: value });
-    //setInputValue(e.target.value);
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      //"http://localhost:8800/commission" - local computer
-      //"http://192.168.1.47:8800/commission" - netwrok
-      feedback.feebackDate = getCurrentDate();
-      //feedback.employerID = commission.
-
-      //feedback.commissionID = fetchLoc().commissionID;
-      const response = await axios.post("http://localhost:8800/rate", feedback);
-      setSuccessMsg(response.data);
-    } catch (err) {
-      console.log(err);
-    }
-
-    console.log("Submitted value:", inputValue);
-    handleCloseModal();
-  };
-
-  // rating //
-  /*const handleStarClick = (value) => {
-    setRating(value === rating ? 0 : value);
-  }; */
-
-  const handleStarClick = (rating) => {
-    // Update feedbackCount based on the star rating clicked
-    setFeedback({ ...feedback, feedbackCount: rating });
-  };
 
   const navigate = useNavigate();
   const location = useLocation();
@@ -112,8 +39,8 @@ const UpdateCommission = () => {
   const { user } = useAuth();
   const userID = user.userID;
   // Add a state to track the marker's longitude and latitude
-  const [markerLngLat, setMarkerLngLat] = useState([123.8854, 10.3157]); // Default values
-  const [currentLocationMarker, setCurrentLocationMarker] = useState(null);
+  // const [markerLngLat, setMarkerLngLat] = useState([123.8854, 10.3157]); // Default values
+  // const [currentLocationMarker, setCurrentLocationMarker] = useState(null);
 
   //handle changes
   const handleChange = (e) => {
@@ -125,32 +52,6 @@ const UpdateCommission = () => {
     } else {
       // For other fields, use spread syntax as before
       setCommission((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-    }
-  };
-
-  //funtion to delete commission
-  const handleDelete = async (commissionID) => {
-    try {
-      //"http://localhost:8800/commission" - local computer
-      //"http://192.168.1.47:8800/commission" - netwrok
-      await axios.delete(`http://localhost:8800/commission/${commissionID}`);
-      window.location.reload();
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  //get the coordinates of the cerrand
-  const fetchLoc = async () => {
-    try {
-      const response = await fetch(
-        `http://localhost:8800/errand/${commissionID}`
-      );
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error(error);
-      return [];
     }
   };
 
@@ -248,6 +149,7 @@ const UpdateCommission = () => {
               methodValue={commission.method}
               number="Contactno"
               numValue={commission.ContactNo}
+              distance={distance}
             />
           </div>
           {commission.comType !== "Delivery" &&
@@ -334,69 +236,9 @@ const UpdateCommission = () => {
         <button className="formButton" onClick={handleClick}>
           UPDATE
         </button>
-        <button className="formButton" onClick={handleOpenModal}>
-          FEEDBACK
-        </button>
-
-        <Modals isOpen={isModalOpen} onClose={handleCloseModal}>
-          <h4>Rate Catcher:</h4>
-          {[1, 2, 3, 4, 5].map((value) => (
-            <span
-              key={value}
-              style={{
-                cursor: "pointer",
-                fontSize: "24px",
-                color: value <= feedback.feedbackCount ? "gold" : "gray",
-              }}
-              onClick={() => handleStarClick(value)}
-            >
-              ★
-            </span>
-          ))}
-          <h4>Feedback:</h4>
-          <input
-            type="text"
-            value={inputValue.feedbackComment}
-            onChange={handleInputChange}
-            placeholder="Enter your comment here...."
-            style={{
-              marginBottom: "10px",
-              width: "100%",
-              padding: "10px",
-              fontSize: "16px",
-            }}
-          />
-
-          <div style={styles.buttonContainer}>
-            <button style={styles.button} onClick={handleSubmit}>
-              Post
-            </button>
-            <button style={styles.button} onClick={handleCloseModal}>
-              Close
-            </button>
-          </div>
-        </Modals>
       </div>
     </div>
   );
-};
-
-const styles = {
-  button: {
-    padding: "10px 20px",
-    fontSize: "16px",
-    backgroundColor: "lightcoral",
-    color: "#fff",
-    border: "none",
-    borderRadius: "5px",
-    cursor: "pointer",
-    marginRight: "10px",
-    textAlign: "center",
-  },
-  buttonContainer: {
-    marginTop: "20px",
-    textAlign: "center",
-  },
 };
 
 export default UpdateCommission;
