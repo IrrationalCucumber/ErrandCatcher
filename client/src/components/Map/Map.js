086de2dca2c4b7b64194378c1faeaaf5f5bbd5ab
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-05-31 12:52:58 +0800

    included Mapbox map

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -4,6 +4,9 @@ import "maplibre-gl/dist/maplibre-gl.css";
 import "./css/map.css";
 import Filter from "./Filter";
 import axios from "axios";
+import mapboxgl from "mapbox-gl";
+import MapboxDirections from "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions";
+import "./css/MapBox.css";
 
 //for map pages
 export default function Map(props) {
@@ -153,6 +156,170 @@ export function MapLibre({ getCoords }) {
   return (
     <div>
       <div ref={mapContainer} className="map-small" />
+    </div>
+  );
+}
+
+//Update errand map
+//for Transpo/Deliveru errand type map
+export function ViewMapBox({
+  accessToken,
+  getDistanceCallback,
+  initialOrigin,
+  initialDestination,
+  interactive,
+}) {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const directions = useRef(null);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+
+    map.current = new mapboxgl.Map({
+      accessToken,
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [initialOrigin.lng, initialOrigin.lat],
+      zoom: 10,
+    });
+
+    map.current.on("load", () => {
+      console.log("Map loaded.");
+
+      directions.current = new MapboxDirections({
+        accessToken,
+        interactive,
+        unit: "metric",
+        controls: {
+          unit: "metric",
+          instructions: false,
+          profileSwitcher: false,
+        },
+      });
+
+      map.current.addControl(directions.current, "top-left");
+
+      if (initialOrigin && initialDestination) {
+        directions.current.setOrigin([initialOrigin.lng, initialOrigin.lat]);
+        directions.current.setDestination([
+          initialDestination.lng,
+          initialDestination.lat,
+        ]);
+      }
+
+      directions.current.on("route", (e) => {
+        if (e.route && e.route.length > 0) {
+          const route = e.route[0];
+          const distance = route.distance;
+          const originCoordinates = route.legs[0].steps[0].maneuver.location;
+          const destinationCoordinates =
+            route.legs[0].steps[route.legs[0].steps.length - 1].maneuver
+              .location;
+          //   console.log(
+          //     "Distance:",
+          //     distance,
+          //     "Origin:",
+          //     originCoordinates,
+          //     "Destination:",
+          //     destinationCoordinates
+          //   );
+          getDistanceCallback(
+            distance,
+            originCoordinates,
+            destinationCoordinates
+          );
+        }
+      });
+    });
+  }, [accessToken, getDistanceCallback, initialOrigin, initialDestination]);
+
+  return (
+    <div className="main-map">
+      <div className="sidebar">
+        Longitude: {initialOrigin.lng} | Latitude: {initialOrigin.lat} | Zoom:
+        10
+      </div>
+      <div
+        ref={mapContainer}
+        className="map-containers"
+        style={{ width: "100%", height: "500px" }}
+      />
+    </div>
+  );
+}
+
+//post errand
+//For transpo/delivery type errand only
+export function PostMapBox({ accessToken, getDistanceCallback }) {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+
+  // Initial state of map, e.g position and zoom
+  const [lng, setLng] = useState(123.8983);
+  const [lat, setLat] = useState(10.2981);
+  const [zoom, setZoom] = useState(16.5);
+
+  const directions = useRef(null);
+  //   const [isReady, setReady] = useState(false);
+  //   const [distance, setDistance] = useState(0);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      accessToken,
+      container: "map-container",
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+
+    map.current.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+
+    // Controller at the top-left corner of the map. Object responsible for origin, destination, distance and etc.
+    directions.current = new MapboxDirections({
+      accessToken,
+      controls: {
+        unit: "metric",
+        instructions: false,
+        profileSwitcher: false,
+      },
+    });
+
+    directions.current.on("route", (e) => {
+      const route = e.route[0];
+
+      //console.log(route);
+
+      // Accessing coordinates of origin and destination
+      const originCoordinates = route.legs[0].steps[0].maneuver.location;
+      const destinationCoordinates =
+        route.legs[0].steps[route.legs[0].steps.length - 1].maneuver.location;
+
+      //getLngLat(startLat, startLng, endLng, endLat);
+      getDistanceCallback(
+        route.distance,
+        originCoordinates,
+        destinationCoordinates
+      );
+
+      // console.log("Origin Coordinates:", originCoordinates);
+      // console.log("Destination Coordinates:", destinationCoordinates);
+    });
+
+    map.current.addControl(directions.current);
+  });
+
+  return (
+    <div className="main-map">
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapContainer} className="map-container" id="map-container" />
     </div>
   );
 }
