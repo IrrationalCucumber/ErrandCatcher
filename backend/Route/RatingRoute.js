acfbbb36a2d450cc244b906aa3dbf88781d2c6e6
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-07-03 13:57:28 +0800

    updated rating route

--- backend/Route/RatingRoute.js
+++ backend/Route/RatingRoute.js
@@ -3,7 +3,7 @@ const ratingController = require("../Controller/RatingController");
 
 const router = express.Router();
 
-router.get("/user-feedbacks", ratingController.getAllFeedback);
+router.get("/all-ratings", ratingController.getAllRatings);
 router.get("/my-feedbacks/:id", ratingController.getFeedbackById); // for catcher
 router.get("/posted-feedbacks/:id", ratingController.getEmployerFeedback); // for emp
 router.get("/user-rating/:id", ratingController.getRatings); // for catch
