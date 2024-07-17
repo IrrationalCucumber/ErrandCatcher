c4549ae58e467310c2ec587ce7179d8c7aa2341a
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-07-17 14:18:12 +0800

    display top rated catcher in a carousel

--- client/src/components/Carousel/TopCatcher.js
+++ client/src/components/Carousel/TopCatcher.js
@@ -4,100 +4,78 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function TopCatcher() {
   const [catchers, setCatchers] = useState([]);
+  //fetch top rated catchers
   useEffect(() => {
     const fetchCatchers = async () => {
       try {
         const res = await axios.get("http://localhost:8800/top-rated");
         setCatchers(res.data);
-      } catch (error) {}
+      } catch (error) {
+        console.error("Error fetching top rated catchers:", error);
+      }
     };
     fetchCatchers();
   }, []);
 
   return (
-    <>
-      {/* <div>TopCatcher</div>
-      {catchers.map((catcher) => (
-        <div key={catcher.feedbackID}>
-          {catcher.username} : {catcher.averageRate}
-        </div>
-      ))} */}
-      <div
-        id="carouselExampleIndicators"
-        className="carousel carousel-dark slide"
-        data-bs-ride="carousel"
-      >
-        {/* 
-          Line Indicators 
-          User can preess to select what to display
-        */}
-        <div className="carousel-indicators">
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="0"
-            className="active"
-            aria-current="true"
-            aria-label="Slide 1"
-            data-bs-ride="carousel"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="1"
-            aria-label="Slide 2"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="2"
-            aria-label="Slide 3"
-          ></button>
-        </div>
-        {/* Item/Cards to display */}
-        <div className="carousel-inner">
-          <div className="carousel-item active" data-bs-interval="5000">
-            <img src="/images/car.png" className="d-block w-50" alt="..." />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>First slide label</h5>
-              <p>
-                Some representative placeholder content for the first slide.
-              </p>
+    <div
+      id="carouselExampleIndicators"
+      className="carousel carousel-dark slide"
+      data-bs-ride="carousel"
+    >
+      {/* 
+        Carusel card/items
+      */}
+      <div className="carousel-inner">
+        {catchers.map((catcher, index) => (
+          <div
+            // set the first item  in the object to be active
+            //
+            className={`carousel-item ${index === 0 ? "active" : ""}`}
+            key={catcher.feedbackID}
+            data-bs-interval="5000"
+          >
+            {/* 
+              temp image
+              Change image to profile or something appropriate
+            */}
+            <img
+              src="/images/catcher.png"
+              className="d-block w-100"
+              alt="..."
+            />
+            {/* 
+              change placeholder texts
+            */}
+            <div className="carousel-caption d-none d-md-block">
+              <h4>{catcher.averageRate}</h4>
+              <h5>{catcher.username}</h5>
+              <p>Some representative placeholder content for the slide.</p>
             </div>
           </div>
-          <div className="carousel-item" data-bs-interval="5000">
-            <img src="/images/hr.png" className="d-block w-50" alt="..." />
-          </div>
-          <div className="carousel-item" data-bs-interval="5000">
-            <img src="/images/img2.png" className="d-block w-50" alt="..." />
-          </div>
-        </div>
-        {/* 
-          Previos and Next button
-        */}
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="prev"
-        >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
+        ))}
       </div>
-    </>
+      {/* 
+          Navigation Control buttons
+        */}
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
   );
 }
