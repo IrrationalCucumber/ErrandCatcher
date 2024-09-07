f87c6607aedc36db3403573ff15b7876dce4454e
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-09-07 08:37:24 +0800

    Initial welcome banner for user

--- client/src/components/HeroSection.js
+++ client/src/components/HeroSection.js
@@ -1,33 +1,19 @@
-import React from 'react'
-import { Button } from './NavButton'
-import { ButtonMap } from './ButtonMap'
-import './HeroSection.css'
-import '../App.css'
+import React from "react";
+import "./HeroSection.css";
+import "../App.css";
 
-function HeroSection() {
+export function HeroSection(props) {
   return (
-    <div className='hero-container'>
-      <video src='/videos/video-test-1.mp4' autoPlay loop muted />
-      <h1>ERRAND CATCHER</h1>
+    <div className="hero-container">
+      <video src="/videos/video-test-1.mp4" autoPlay loop muted />
+      <h1>
+        WELCOME BACK, {props.type} {props.username}
+      </h1>
       <p>See what errand is available</p>
-      <div className='hero-btns'>
-        <Button 
-            className='btns' 
-            buttonStyle='btn--outline'
-            buttonSize='btn--large'
-            >
-            GET STARTED
-        </Button>
-        <ButtonMap 
-            className='btns' 
-            buttonStyle='btn--primary'
-            buttonSize='btn--large'
-            >
-            VIEW NEARBY COMMISSION
-        </ButtonMap>
-        </div> 
     </div>
-  )
+  );
 }
 
-export default HeroSection
+export function bannerHeroSection() {
+  return <div>Hi</div>;
+}
