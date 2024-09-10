4718a001ee8ea1a6dc80f781ff4ae1f29f12efe8
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-09-10 12:52:16 +0800

    Adjusted ui for banner

--- client/src/Banner/HeroSection.js
+++ client/src/Banner/HeroSection.js
@@ -5,11 +5,10 @@ import "../App.css";
 export function HeroSection(props) {
   return (
     <div className="hero-container">
-      <video src="/videos/video-test-1.mp4" autoPlay loop muted />
       <h1>
-        WELCOME BACK, {props.type} {props.username}
+        {props.type} {props.username}
       </h1>
-      <p>See what errand is available</p>
+      <p>WELCOME BACK!</p>
     </div>
   );
 }
