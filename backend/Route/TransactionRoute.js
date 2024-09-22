4434d4dcb2dbb013ce856ba55d292bc4af54f558
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-09-22 20:53:56 +0800

    Update transaction endpoint

--- backend/Route/TransactionRoute.js
+++ backend/Route/TransactionRoute.js
@@ -12,7 +12,7 @@ router.get("/accepted-errand/:id", transConstroller.getTransById);
 router.get("/catcher/ongoing/:id", transConstroller.getOngoing);
 router.get("/catcher/cancelled/:id", transConstroller.getCancelled);
 router.get("/pending-errands/:id", transConstroller.getTakenErrand);
-router.get("/employer/ongoing/:id", transConstroller.getOngoingErrand);
+router.get("/employer/ongoing/:id", transConstroller.getOngoingErrand); // not included "complete" status
 router.get("/employer/cancelled/:id", transConstroller.getCancelledErrand);
 router.post("/add-trans/", transConstroller.postNewTrans);
 router.put("/cancel-trans/:id", transConstroller.putCancelTrans);
