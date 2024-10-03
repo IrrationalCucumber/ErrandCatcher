4b246107dd6ed02e6b710ef08d08a70e37434d7a
markdannim <markdanimmm@gmail.com>
2024-10-03 14:19:27 +0800

    Minor change on radioinputs

--- client/src/pages/SignUp/RadioInputs.js
+++ client/src/pages/SignUp/RadioInputs.js
@@ -1,3 +1,4 @@
+/*
 import React from "react";
 
 function RadioInputs({ options, selectedOption, onChange }) {
@@ -26,8 +27,8 @@ function RadioInputs({ options, selectedOption, onChange }) {
               <div className="separator"></div>
               <div className="radio-description">
                 <h3>
-                  {/* I'm {option.value === "employer" ? "an employer" : "a catcher"},
-                  looking for {option.value === "employer" ? "catcher" : "errands"}. */}
+                  //I'm {option.value === "employer" ? "an employer" : "a catcher"},
+                  //looking for {option.value === "employer" ? "catcher" : "errands"}. 
                   I'm{" "}
                   {option.value === "Employer" ? "an employer" : "a catcher"},
                   looking for{" "}
@@ -135,3 +136,133 @@ function RadioInputs({ options, selectedOption, onChange }) {
 }
 
 export default RadioInputs;
+
+*/
+
+import React from "react";
+
+function RadioInputs({ options, selectedOption, onChange }) {
+  return (
+    <div className="radio-container">
+      <div className="grid-container">
+        {options.map((option, index) => (
+          <div className="grid-item" key={index}>
+            <div
+              className={`radio-wrapper ${
+                selectedOption === option.value ? "selected" : ""
+              }`}
+              onClick={() => onChange({ target: { value: option.value } })}
+            >
+              <div className="radio-header">
+                <input
+                  type="radio"
+                  id={option.value}
+                  value={option.value}
+                  checked={selectedOption === option.value}
+                  onChange={onChange}
+                />
+                <label htmlFor={option.value} className="radio-label">
+                  {option.label}
+                </label>
+              </div>
+              <div className="separator"></div>
+              <div className="radio-description">
+                <h3>
+                  I'm{" "}
+                  {option.value === "Employer" ? "an employer" : "a catcher"},
+                  looking for{" "}
+                  {option.value === "Employer" ? "catcher" : "errands"}.
+                </h3>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <style>
+        {`
+          .radio-container {
+            margin-bottom: 20px;
+          }
+          
+          .grid-container {
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            grid-gap: 20px;
+          }
+          
+          .grid-item {
+            margin-bottom: 20px;
+          }
+          
+          .radio-wrapper {
+            border: 1px solid #0073aa;
+            border-radius: 5px;
+            padding: 10px;
+            cursor: pointer;
+            transition: all 0.3s ease;
+          }
+
+          .radio-wrapper.selected {
+            background-color: #0073aa;
+            color: #fff;
+          }
+
+          .radio-header {
+            display: flex;
+            align-items: center;
+            margin: 10px;
+          }
+
+          .radio-header.selected {
+            background-color: #0073aa;
+            color: #fff;
+          }
+
+          .radio-header.selected .radio-label {
+            color: white;
+          }
+          
+          .radio-header input[type="radio"] {
+            display: none;
+          }
+          
+          .radio-label {
+            margin-left: 10px;
+            color: black;
+            font-size: 1.3rem; 
+          }
+          
+          .separator {
+            border-bottom: 1px solid #ccc;
+            margin: 10px 0;
+          }
+          
+          .radio-description {
+            margin: 20px;
+          }
+
+          @media screen and (max-width: 964px) {
+            .radio-wrapper {
+              height: 250px;
+              display: flex;
+              flex-direction: column;
+            }
+
+            .radio-description {
+              height: 500px;
+              display: flex;
+              justify-content: center;
+            }
+
+            .radio-description h3 {
+              font-size: 1.5rem;
+            }
+          }
+        `}
+      </style>
+    </div>
+  );
+}
+
+export default RadioInputs;
+
