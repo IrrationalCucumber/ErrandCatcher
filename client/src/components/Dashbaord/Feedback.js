d09477b22228e4abc471d21b2823386d18394e5f
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-10-15 14:28:45 +0800

    Initial UI for Feedback contents

--- client/src/components/Dashbaord/Feedback.js
+++ client/src/components/Dashbaord/Feedback.js
@@ -6,6 +6,16 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../AuthContext";
+import {
+  Card,
+  CardContent,
+  CardCover,
+  CardOverflow,
+  List,
+  ListItem,
+} from "@mui/joy";
+import StarRating from "../Display/StarRating";
+import { DisplayDate } from "../DisplayDate";
 
 function Feedback() {
   return <div></div>;
@@ -24,7 +34,9 @@ export function MyFeedback() {
     //fetch data in backend
     const fetchFeedback = async () => {
       try {
-        const res = await axios.get(`http://localhost:8800/${user.userID}`);
+        const res = await axios.get(
+          `http://localhost:8800/my-feedbacks/${user.userID}`
+        );
         setMyFeedback(res.data);
       } catch (error) {
         console.log(error);
@@ -34,5 +46,37 @@ export function MyFeedback() {
     const refresh = setInterval(fetchFeedback, 5000);
     return () => clearInterval(refresh);
   }, []);
-  return <></>;
+  return (
+    <>
+      {myFeedback.map((feedback) => (
+        <List key={feedback.feedbackID}>
+          <ListItem>
+            <Card
+              invertedColors={false}
+              orientation="horizontal"
+              size="lg"
+              variant="soft"
+              sx={{
+                width: "90%",
+                "--Card-radius": "0px",
+                "--Card-padding": "20px",
+                "&:hover": {
+                  boxShadow: "md",
+                  borderColor: "neutral.outlinedHoverBorder",
+                },
+              }}
+            >
+              <CardContent>
+                <StarRating rating={feedback.feedbackRate} />
+                <i>
+                  "{feedback.feedbackComment}" <b>-{feedback.username}</b>
+                </i>
+                {DisplayDate(feedback.feedbackDate)}
+              </CardContent>
+            </Card>
+          </ListItem>
+        </List>
+      ))}
+    </>
+  );
 }
