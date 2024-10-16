963f34a5eb42761f5ecdd6e13673696bd6a3a8d0
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-10-16 09:50:03 +0800

    Add feedback list component fo employer

--- client/src/components/Dashbaord/Feedback.js
+++ client/src/components/Dashbaord/Feedback.js
@@ -73,3 +73,60 @@ export function MyFeedback() {
     </>
   );
 }
+
+/**
+ * FEEDBACK LIST for EMployer
+ */
+export function MyPostedFeedback() {
+  const { user } = useAuth();
+  const [feedback, setFeedback] = useState([]);
+
+  //fetch posted feedback of employer
+  useEffect(() => {
+    const fetchFeedback = async () => {
+      try {
+        const res = await axios.get(``);
+        setFeedback(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    //refresh list every 5 sec
+    const refresh = setInterval(fetchFeedback, 5000);
+    return () => clearInterval(refresh);
+  }, []);
+  return (
+    <>
+      {feedback.map((fb) => {
+        <List key={fb.feedbackID}>
+          <ListItem>
+            <Card
+              invertedColors={false}
+              orientation="horizontal"
+              size="lg"
+              variant="soft"
+              sx={{
+                width: "100%",
+                "--Card-radius": "0px",
+                "--Card-padding": "20px",
+                "&:hover": {
+                  boxShadow: "md",
+                  borderColor: "neutral.outlinedHoverBorder",
+                },
+              }}
+            >
+              <CardContent>
+                <StarRating rating={feedback.feedbackRate} />
+                {/* CHANGE TO CATCHER */}
+                <i>
+                  "{feedback.feedbackComment}" <b>-{feedback.username}</b>
+                </i>
+                {DisplayDate(feedback.feedbackDate)}
+              </CardContent>
+            </Card>
+          </ListItem>
+        </List>;
+      })}
+    </>
+  );
+}
