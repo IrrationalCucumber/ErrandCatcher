c259adfe786b84bdfbdb5c829cc02fdbbe7a6046
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-10-25 11:55:23 +0800

    Update file name for universal usage

--- client/src/components/Display/DsiplayFunctions.js
+++ client/src/components/Display/DsiplayFunctions.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Capitalize(text) {
+export function Capitalize(text) {
   const capitalizeThis = text;
   return capitalizeThis.charAt(0).toUpperCase() + capitalizeThis.slice(1);
 }
