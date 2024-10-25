644243656f38fc736de7037d02e0a47283057919
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-10-25 12:06:11 +0800

    Add function for displaying date

--- client/src/components/Display/DsiplayFunctions.js
+++ client/src/components/Display/DsiplayFunctions.js
@@ -4,3 +4,29 @@ export function Capitalize(text) {
   const capitalizeThis = text;
   return capitalizeThis.charAt(0).toUpperCase() + capitalizeThis.slice(1);
 }
+
+export function DisplayDate(theDate) {
+  //Display format to date
+  // months into words
+
+  const date = new Date(theDate);
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ]; // Get the month and year from the date object
+  const month = monthNames[date.getMonth()];
+  const year = date.getFullYear();
+
+  // Construct the formatted date string
+  return `${month} ${date.getDate()}, ${year}`;
+}
