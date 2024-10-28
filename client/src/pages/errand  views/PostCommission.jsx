c2a364132d78b6d24ac1bd472953a40b9c5c84e7
Mamonde02 <valerosojohnraymund@gmail.com>
2024-10-28 10:31:56 +0800

    minor delay timeout

--- client/src/pages/errand  views/PostCommission.jsx
+++ client/src/pages/errand  views/PostCommission.jsx
@@ -176,8 +176,8 @@ const PostCommission = () => {
         setAlerMsg("Looks like you havent set the location in the Map");
         setShowAlert(true);
       } else {
-        await axios.post("http://localhost:8800/commission", updatedCommission);
-        await axios.post("http://localhost:8800/notify-catcher");
+        // await axios.post("http://localhost:8800/commission", updatedCommission);
+        // await axios.post("http://localhost:8800/notify-catcher");
 
         // alert("You have Posted an Errand!");
         // navigate(`/dashboard/commissions`);
@@ -191,8 +191,14 @@ const PostCommission = () => {
           // alert("You have Posted an Errand!");
           // navigate(`/dashboard/commissions`);
 
+          setTimeout(() => {
+            // setLoading(false);
+            navigate(`/dashboard/commissions`);
+
+          }, 1900);
+
         }, 2000);
-        // setOpen(true);
+
       }
     } catch (err) {
       console.log(err);
