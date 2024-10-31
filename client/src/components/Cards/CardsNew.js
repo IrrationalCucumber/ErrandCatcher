b4a8a4c7818a3a461769791f3056189260a3a8b6
Mamonde02 <valerosojohnraymund@gmail.com>
2024-10-31 21:30:21 +0800

    new cards component

---
+++ client/src/components/Cards/CardsNew.js
@@ -0,0 +1,58 @@
+/**
+ * WRITTEN BY: META AI FT MONDE
+ * 10/31/24
+ * Component display preview Cards
+ */
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import CardItemNew from "./CardsItemNew";
+import axios from "axios";
+import "./cardsNew.css";
+import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
+
+function CardsNew() {
+    const [commissions, setCommissions] = useState([]);
+
+    //rretrieve data
+    useEffect(() => {
+        const fetchAllCommission = async () => {
+            try {
+                const res = await axios.get("http://localhost:8800/available");
+                //"http://localhost:8800/commission" - local computer
+                //"http://192.168.1.47:8800/commission" - netwrok
+                setCommissions(res.data);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        fetchAllCommission();
+    }, []);
+    const location = useLocation();
+    //pathname to array from
+    //get the id
+    const userID = location.pathname.split("/")[2];
+
+    return (
+        <div className="cards">
+            {/* <h1>Check out this epic Destination!</h1> */}
+            <div className="cards__container">
+                <div className="cards__wrapper">
+                    <div className="cards__items">
+                        {commissions.map((commission) => (
+                            <CardItemNew
+                                key={commission.commissionID}
+                                items={<DirectionsCarIcon sx={{ color: '#fff', fontSize: 100 }} />}
+                                title={commission.commissionTitle}
+                                type={commission.commissionType}
+                                location={commission.commissionLocation}
+                                path={`/errand/view-errand/${commission.commissionID}`}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CardsNew;
