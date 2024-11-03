0e03a5cab3b0ef868f457ae3f8680a62b95c6f9e
Mamonde02 <valerosojohnraymund@gmail.com>
2024-11-03 11:25:50 +0800

    create new component ongoing cards

---
+++ client/src/pages/Dropdown/OngoingCardsNew.js
@@ -0,0 +1,124 @@
+/**
+ * WRITTEN BY: MONDE
+ * 3/11/24
+ * Component display preview OngoingCards v1.2
+ */
+import React from "react";
+import { Link } from "react-router-dom";
+import OtherHousesIcon from '@mui/icons-material/OtherHouses';
+import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import { Box } from '@mui/joy';
+import {
+    Button,
+    Divider,
+    DialogTitle,
+    DialogContent,
+    DialogActions,
+    Modal,
+    ModalDialog,
+    Chip,
+    Typography,
+} from "@mui/joy";
+import "../../components/Cards/cardsNew.css";
+
+function OngoingCardsNew(props) {
+
+    return (
+        <>
+            <div class="card">
+                <div class="iconcard" >
+                    <Box class="boxer">
+                        {/* commissionType props */}
+                        {props.icon === "HomeService - Indoor" ||
+                            props.icon === "HomeService - Outdoor" ?
+                            (
+                                <OtherHousesIcon
+                                    sx={{ color: '#fff', fontSize: 100 }}
+                                />
+                            ) : props.icon === "Transportation" ? (
+                                <LocalShippingIcon
+                                    sx={{ color: '#fff', fontSize: 100 }}
+                                />
+                            ) : props.icon === "Delivery" ? (
+                                <DirectionsCarIcon
+                                    sx={{ color: '#fff', fontSize: 100 }}
+                                />
+                            ) : null}
+                    </Box>
+                </div>
+                <div class="contentcard">
+
+
+                    <span class="title">
+                        {/* {props.title} */}
+                        <Typography level="h4" color="neutral" variant="plain">
+                            {/* {commission.commissionTitle} */}
+                            {props.title}
+                        </Typography>
+                    </span>
+
+                    <p class="desc">
+                        {/* props.desc */}
+                        {/* {props.type} */}
+                        <Typography className="ongoing__cards__txt" level="body-sm">
+                            {/* {commission.commissionType} */}
+                            {props.type}
+                        </Typography>
+
+                    </p>
+
+                    <p class="desc">
+                        {/* props.desc */}
+                        <h7>Details: </h7>
+                        <ul>
+                            <li>
+                                <Chip color="success" size="lg" variant="outlined">
+                                    {/* {commission.errandStatus} */}
+                                    {props.status}
+                                </Chip>
+                            </li>
+                            <li>
+                                <Typography className="ongoing__cards__txt" level="title-sm">
+                                    Payment:
+                                    <Typography
+                                        color="success"
+                                        level="title-sm"
+                                        variant="outlined"
+                                    >
+                                        {/* Php {commission.commissionPay} */}
+                                        ₱{props.pay}
+                                    </Typography>
+                                </Typography>
+                            </li>
+                        </ul>
+                    </p>
+
+                    {/* <Typography
+                        className="ongoing__cards__txt"
+                        color="primary"
+                        level="title-sm"
+                    >
+                        <Link
+                            // to={`/errand/view-errand/${commission.commissionID}`}
+                            className="Oncard__link"
+                        >
+                            View Errand
+                        </Link>
+                    </Typography> */}
+
+                    <Link to={props.path}>
+                        <a class="action" href="#">
+                            Find out more
+                            <span aria-hidden="true">
+                                →
+                            </span>
+                        </a>
+                    </Link>
+                </div >
+            </div>
+        </>
+    );
+}
+
+export default OngoingCardsNew;
