94a5c6e11d5aea01d466a4f14ffe16ff79dbb0b1
Mamonde02 <valerosojohnraymund@gmail.com>
2024-11-16 10:28:50 +0800

    initial payment success ui

--- client/src/pages/PaymentSuccess.jsx
+++ client/src/pages/PaymentSuccess.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import HomeIcon from '@mui/icons-material/Home';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CheckCircleOutlineOutlinedIcon from '@mui/icons-material/CheckCircleOutlineOutlined';
 
 const PaymentSuccess = () => {
 
@@ -12,19 +13,57 @@ const PaymentSuccess = () => {
             alignContent: "center",
             justifyContent: "center",
             textAlign: "center",
-            marginTop: "200px",
+            marginTop: "120px",
             padding: "12px",
         }}>
-            <div style={{ display: "inline-block", margin: "0 auto" }}>
 
-                <h1>Payment has been Succesful! <CheckCircleIcon color="success" /> </h1>
-                <h3>
-                    Click here to go back <Link to={"/dashboard/home"}><HomeIcon /> HOME</Link>
+            <div style={{
+                display: "inline-block",
+                margin: "0 auto",
+                border: "0.666667px solid #d6d6d6",
+                borderRadius: "15px",
+                padding: "60px",
+                // boxShadow: "0 2px 5px rgba(0, 0, 0, 0.1)",
+                boxShadow: "#00000012 0px 9px 20px 0px",
+            }}>
+                <CheckCircleOutlineOutlinedIcon
+                    color="success"
+                    sx={{
+                        fontSize: 80,
+                    }} />
+                <h1 style={{
+                    fontWeight: "500",
+                    lineHeight: "44px",
+                    margin: "20px 0px 10px",
+                    // fontSize: "38px",
+                    fontSize: "2.55rem",
+                }}
+                >
+                    Payment has been Succesful!
+                </h1>
+
+                <h3 style={{
+                    lineHeight: "24px",
+                    margin: "0px 0px 20px"
+                }}
+                >
+                    Click here to go back
+
+                    <div style={{
+                        border: "solid",
+                        borderRadius: "50px",
+                        padding: "12px",
+                        marginTop: "20px",
+                        background: "#0073aa",
+                        color: "white",
+                    }}>
+                        <Link to={"/dashboard/home"}><HomeIcon /> HOME</Link>
+                    </div>
                 </h3>
 
             </div>
 
-        </div>
+        </div >
     );
 };
 
