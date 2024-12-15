fd78c4f7a6f898a99ecb1ed47bde71ed0bcbce39
Mamonde02 <valerosojohnraymund@gmail.com>
2024-12-15 18:20:13 +0800

    naviagate pending

--- client/src/components/Employer Cards/EmployerCards.js
+++ client/src/components/Employer Cards/EmployerCards.js
@@ -96,7 +96,7 @@ const EmployerCard = ({ employer }) => {
                 <div class="card-block">
                   <Link
                     style={{ textDecoration: "none" }}
-                    to={`/dashboard/applicants`}
+                    to={`/dashboard/errands/${userID}`}
                   >
                     <h3 class="m-b-20 fw-semibold"> <PendingActionsIcon sx={{ color: "white", fontSize: 24, }} /> Pending</h3>
                     <h2 class="text-right">
