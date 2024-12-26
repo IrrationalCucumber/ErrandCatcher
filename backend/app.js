acfbd4e131fcca2a187f2ed929eac1b2a8f16637
Mamonde02 <valerosojohnraymund@gmail.com>
2024-12-26 23:10:00 +0800

    modifed status query

--- backend/app.js
+++ backend/app.js
@@ -199,7 +199,7 @@ app.get("/success-payment/:id", (req, res) => {
   const currentTime = new Date().toISOString().slice(0, 19).replace("T", " ");
 
   const q1 = `UPDATE errandtransaction 
-            SET errandStatus = 'Task Done', transDateComplete = ? 
+            SET transStatus = 'Task Done', transDateComplete = ? 
             WHERE transactID = ?`;
 
   const q2 = `INSERT INTO invoice (total, type, description, checkoutId, paymentId, paid, invoiceErrandID, invoiceemployerID, invoiceCatcherID) VALUES ( ?, ?, ?, ?, ?, FROM_UNIXTIME(?), ?, ?, ? )`;
