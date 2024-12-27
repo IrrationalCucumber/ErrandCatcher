e85030bd2bc6ff85dae4f23e8a6eb0a141a823b1
Mamonde02 <valerosojohnraymund@gmail.com>
2024-12-27 12:13:52 +0800

    revert conflict searchpage

--- client/src/components/Search Bar/SearchBar.js
+++ client/src/components/Search Bar/SearchBar.js
@@ -8,37 +8,39 @@ function SearchBar(props) {
   const navigate = useNavigate();
 
   return (
-    <div className="search__bar__container">
-      {props.hasErrand === "false" ? (
-        <h1>Search what errands you want</h1>
-      ) : (
-        <h1>You still have an Errands to do!</h1>
-      )}
-
-      {props.hasErrand === "false" ? (
-        <div className="group">
-          <SearchIcon className="icon" />
-          <input
-            className="inputsss"
-            type="text"
-            placeholder="Search"
-            value={props.value}
-            onChange={props.onChange}
-          />
-          <button className="buttonss" onClick={props.onClick}>
-            Search
-          </button>
-        </div>
-      ) : (
-        <Button
-          onClick={() => navigate(`/dashboard/catcher-errands`)}
-          size="lg"
-          variant="soft"
-        >
-          VIEW ERRANDS
-        </Button>
-      )}
-    </div>
+    <form onSubmit={props.onClick}>
+      <div className="search__bar__container">
+        {props.hasErrand === "false" ? (
+          <>
+            <h1>Search what errands you want</h1>
+            <div className="group">
+              <SearchIcon className="icon" />
+              <input
+                className="inputsss"
+                type="text"
+                placeholder="Search"
+                value={props.value}
+                onChange={props.onChange}
+              />
+              <button className="buttonss" type="submit">
+                Search
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+            <h1>You still have an Errands to do!</h1>
+            <Button
+              onClick={() => navigate(`/dashboard/catcher-errands`)}
+              size="lg"
+              variant="soft"
+            >
+              VIEW ERRANDS
+            </Button>
+          </>
+        )}
+      </div>
+    </form>
   );
 }
 
