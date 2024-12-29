8a1ca25fbb9f4b8819d5a2f11fabbcc6b4a25c51
Mamonde02 <valerosojohnraymund@gmail.com>
2024-12-29 14:16:07 +0800

    add condition for admin menu display

--- client/src/pages/Menu.jsx
+++ client/src/pages/Menu.jsx
@@ -76,7 +76,7 @@ const Menu = () => {
         icons={<HourglassBottomIcon />}
       />
 
-      {user.hasErrand === "false" && (
+      {user.hasErrand === "false" || user.userType === "admin" || user.userType === "Catcher" ? (
         <section className="Menu" id="Menu">
           {/* bootstrap class applied */}
           <div className="box-container d-flex justify-content-center align-items-center">
@@ -131,7 +131,7 @@ const Menu = () => {
             </div>
           </div>
         </section>
-      )}
+      ) : null}
     </>
   );
 };
